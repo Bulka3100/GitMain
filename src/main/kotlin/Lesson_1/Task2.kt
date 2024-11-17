58406aeffa41cfc6f142543b46680b8ac53dffd9
Breaker <kirillvip3100@yandex.ru>
2024-11-18 02:43:49 +0300

    GM-1-2-problems-solved

--- src/main/kotlin/Lesson_1/Task2.kt
+++ src/main/kotlin/Lesson_1/Task2.kt
@@ -1,14 +1,12 @@
 package org.example.Lesson_1
 
-fun main(){
-    val NumberOfOrders:Int=75
-    val GratefulMessage: String= "Спасибо за покупку"
+fun main() {
+    val NumberOfOrders: Int = 75
+    val GratefulMessage: String = "Спасибо за покупку"
     println(NumberOfOrders)
     println(GratefulMessage)
-    var NumberOfWorkers:Int=2000
+    var NumberOfWorkers: Int = 2000
 //    println(NumberOfWorkers)
-    NumberOfWorkers=1999
+    NumberOfWorkers = 1999
     println(NumberOfWorkers)
-
-
 }
