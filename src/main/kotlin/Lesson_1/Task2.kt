1c7d7eeb3f8563fa37dfc311779d5a3ac1c60bb5
Breaker <kirillvip3100@yandex.ru>
2024-11-17 18:34:30 +0300

    GM 1-2 do WorkersNumber

---
+++ src/main/kotlin/Lesson_1/Task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_1
+
+fun main(){
+    val NumberOfOrders:Int=75
+    val GratefulMessage: String= "Спасибо за покупку"
+    println(NumberOfOrders)
+    println(GratefulMessage)
+    var NumberOfWorkers:Int=2000
+//    println(NumberOfWorkers)
+    NumberOfWorkers=1999
+    println(NumberOfWorkers)
+
+
+}
