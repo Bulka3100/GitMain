fd74866211a025032a46e3c8f399c13a11636d43
Breaker <kirillvip3100@yandex.ru>
2024-11-18 02:33:35 +0300

    GM-1-1-problems-solved

--- src/main/kotlin/Lesson_1/Task1.kt
+++ src/main/kotlin/Lesson_1/Task1.kt
@@ -1,6 +1,6 @@
 package org.example.Lesson_1
 
-fun main(){
-    val NumberOfOrders:Int=75
-    val GratefulMessage: String= "Спасибо за покупку"
+fun main() {
+    val NumberOfOrders: Int = 75
+    val GratefulMessage: String = "Спасибо за покупку"
 }
