5623d5dbcccff966f72b2a09fb36306f7431ad7b
Breaker <kirillvip3100@yandex.ru>
2024-11-18 21:22:31 +0300

    KS-3-3-task2-done

--- src/main/kotlin/Lesson_3/task3.kt
+++ src/main/kotlin/Lesson_3/task3.kt
@@ -1,2 +1,19 @@
 package org.example.Lesson_3
 
+fun main(){
+    val nubmer = 3
+    println("""
+      |$nubmer*1=${nubmer*1}
+      |$nubmer*2=${nubmer*2}
+      |$nubmer*3=${nubmer*3}
+      |$nubmer*4=${nubmer*4}
+      |$nubmer*5=${nubmer*5}
+      |$nubmer*6=${nubmer*6}
+      |$nubmer*7=${nubmer*7}
+      |$nubmer*8=${nubmer*8}
+      |$nubmer*9=${nubmer*9}
+      
+        
+  """.trimMargin()  )
+
+}
