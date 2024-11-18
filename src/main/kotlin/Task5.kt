ac72b158d4287722e3c7281e46de1ed77730ec05
Breaker <kirillvip3100@yandex.ru>
2024-11-18 03:15:24 +0300

    KS-1-5-problems-solved

--- src/main/kotlin/Task5.kt
+++ src/main/kotlin/Task5.kt
@@ -1,18 +1,15 @@
 package org.example.Lesson_1
-fun main(){
 
-    val quantityOfSeconds: Short=6485
-    val minutes = (quantityOfSeconds/60)-60 // понимаем минут остаётся
-    var hours: Int= quantityOfSeconds/60/60
-    var seconds:Int= quantityOfSeconds- minutes*60 -hours*3600
-    /* хотел вот так сделать для отображения минут и часов 2мя цифрами. Выдаёт ошибку
-        if (hours<10) { hours.toString()
-        hours= "0$hours"} else
-        hours (через when не получилось ): )*/
+fun main() {
 
-//    я могу написать конструкцию с if для общего решения, но мне кажется вы этого не предусматривали. Интересно посмотреть ваше решение
+    val quantityOfSeconds: Short = 6485
+    val minutes = (quantityOfSeconds / 60) - 60 // понимаем минут остаётся
+    var hours: Int = quantityOfSeconds / 60 / 60
+    var seconds: Int = quantityOfSeconds - minutes * 60 - hours * 3600
 
-   println("время в полёте 0$hours:$minutes:0$seconds")
+
+
+    println("0$hours:$minutes:0$seconds")
 
 }
 
