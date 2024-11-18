baf906eb7fbe546a36aef3a3456b493152b66147
Breaker <kirillvip3100@yandex.ru>
2024-11-18 11:58:34 +0300

    KS-2-1-task-done

--- src/main/kotlin/Lesson_2/Task1.kt
+++ src/main/kotlin/Lesson_2/Task1.kt
@@ -1,5 +1,10 @@
 package org.example.Lesson_2
 
-fun main(){
-
+fun main() {
+    val student1 = 3.0
+    val student2 = 4.0
+    val student3 = 3.0
+    val student4 = 5.0
+    val mean: Double = (student1+student2+student3+student4)/4
+    println(mean)
 }
