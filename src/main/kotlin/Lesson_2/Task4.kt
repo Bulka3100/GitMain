c68d467cddfec4fb0f7dc13c3da18258b8908f52
Breaker <kirillvip3100@yandex.ru>
2024-11-18 13:45:15 +0300

    KS-2-4-task-done

--- src/main/kotlin/Lesson_2/Task4.kt
+++ src/main/kotlin/Lesson_2/Task4.kt
@@ -1,5 +1,11 @@
 package org.example.Lesson_2
 
-fun main(){
+fun main() {
+    val crystalOreMined = 7
+    val ironOreMined = 11
+    val crystalOreMinedWithBuff = ((crystalOreMined * 1.2) - crystalOreMined).toInt()
+    val IronOreMinedWithBuff = ((ironOreMined * 1.2) - ironOreMined).toInt()
 
+    println(crystalOreMinedWithBuff)
+    println(IronOreMinedWithBuff)
 }
