59c725a3b40237049989a2fcbc17d1dccd12fe2f
Breaker <kirillvip3100@yandex.ru>
2024-11-18 14:46:02 +0300

    KS-2-5-task-done

--- src/main/kotlin/Lesson_2/Task5.kt
+++ src/main/kotlin/Lesson_2/Task5.kt
@@ -1,5 +1,12 @@
 package org.example.Lesson_2
 
-fun main(){
+import kotlin.math.pow
 
+fun main() {
+    val summ = 70_000
+    val procent = 16.7
+    val time = 20
+    val result = (summ * ((1 + procent / 100).pow(time))).toDouble()
+    val formatResult = Math.round(result * 1000.0) / 1000.0
+    println(formatResult)
 }
