74fe0e9dd808ae5ee7ed24b25102f86ea0a55954
Breaker <kirillvip3100@yandex.ru>
2024-11-18 21:40:55 +0300

    KS-3-4-task4-done

--- src/main/kotlin/Lesson_3/task4.kt
+++ src/main/kotlin/Lesson_3/task4.kt
@@ -1,2 +1,12 @@
 package org.example.Lesson_3
 
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var actionNumber = 1
+    println("$from-$to;$actionNumber")
+    from = "D2"
+    to = "D3"
+    actionNumber++
+    println("$from-$to;$actionNumber")
+}
