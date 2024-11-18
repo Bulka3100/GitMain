152a04151a8fdcb9292dd860978b53dcb11d6e20
Breaker <kirillvip3100@yandex.ru>
2024-11-18 20:43:47 +0300

    KS-3-1-task1-done

--- src/main/kotlin/Lesson_3/task1.kt
+++ src/main/kotlin/Lesson_3/task1.kt
@@ -1,2 +1,9 @@
 package org.example.Lesson_3
 
+fun main() {
+    val GreetingsMorning = "Доброе утро"
+    val GreetingsAfternoon = "Добрый день"
+    val userName = "Николай"
+    println("$GreetingsMorning,$userName")
+    println("$GreetingsAfternoon,$userName")
+}
