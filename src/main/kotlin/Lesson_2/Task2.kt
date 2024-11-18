603a5a7f2c62f603cc7593fcbe2e6d2f44ec9723
Breaker <kirillvip3100@yandex.ru>
2024-11-18 12:10:45 +0300

    KS-2-1-task-done

--- src/main/kotlin/Lesson_2/Task2.kt
+++ src/main/kotlin/Lesson_2/Task2.kt
@@ -1,5 +1,15 @@
 package org.example.Lesson_2
 
-fun main(){
+fun main() {
+        val commonWorkers = 50
+        val commonWorkersSalary = 30_000
+        val traineeWorkers = 30
+        val traineeWorkersSalary = 20_000
 
+        val constWorkersSalary: Int = commonWorkers*commonWorkersSalary
+        val commonSalary = constWorkersSalary+traineeWorkersSalary*traineeWorkers
+        val meanSalary = commonSalary/(commonWorkers+traineeWorkers)
+        println(constWorkersSalary)
+        println(commonSalary)
+        println(meanSalary)
 }
