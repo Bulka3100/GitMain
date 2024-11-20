193b1c6e7cc8921f8bf927fe6d905678b5f6a071
Breaker <kirillvip3100@yandex.ru>
2024-11-21 01:34:32 +0300

    KS-2-1-final

--- src/main/kotlin/Lesson_2/Task1.kt
+++ src/main/kotlin/Lesson_2/Task1.kt
@@ -5,8 +5,8 @@ fun main() {
     val student2 = 4.0
     val student3 = 3.0
     val student4 = 5.0
-    val mean: Double = (student1 + student2 + student3 + student4) / NUMBER0FSTUDENTS
+    val mean: Double = (student1 + student2 + student3 + student4) / NUMBER_0F_STUDENTS
     println(mean)
 }
 
-const val NUMBER0FSTUDENTS = 4
+const val NUMBER_0F_STUDENTS = 4
