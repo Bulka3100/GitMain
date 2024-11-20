cf102e2bddd680e0aaa2b540ccd964505cec8783
Breaker <kirillvip3100@yandex.ru>
2024-11-20 04:12:22 +0300

    KS-2-1-Marks

--- src/main/kotlin/Lesson_2/Task1.kt
+++ src/main/kotlin/Lesson_2/Task1.kt
@@ -5,6 +5,7 @@ fun main() {
     val student2 = 4.0
     val student3 = 3.0
     val student4 = 5.0
-    val mean: Double = (student1+student2+student3+student4)/4
+    val mean: Double = (student1+student2+student3+student4)/ NumberOfStudents
     println(mean)
 }
+const val NumberOfStudents = 4
