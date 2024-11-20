c184cfd02c5dc6361aecb9c9671baa4b5313b739
Breaker <kirillvip3100@yandex.ru>
2024-11-21 01:56:42 +0300

    KS-1-5-res

--- src/main/kotlin/Task5.kt
+++ src/main/kotlin/Task5.kt
@@ -3,11 +3,12 @@ package org.example.Lesson_1
 fun main() {
 
     val quantityOfSeconds: Short = 6480
-    val minutes = (quantityOfSeconds / inMinute) - inMinute // понимаем минут остаётся
+    val minutes = (quantityOfSeconds / inMinute) - inMinute
     var hours: Int = quantityOfSeconds / inMinute / inMinute
     var seconds: Int = quantityOfSeconds - minutes * inMinute - hours * inMinute * inMinute
-    println("${String.format("%02d", hours)}:${String.format("%02d", minutes)}:${String.format("%02d", seconds)}")
+    println("%02d:%02d:%02d".format(hours,minutes,seconds))
 
 }
+
 const val inMinute = 60
 
