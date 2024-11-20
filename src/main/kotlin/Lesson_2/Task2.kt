6eef2d80e0e9cee35afccab1b3caa863affe42cf
Breaker <kirillvip3100@yandex.ru>
2024-11-20 17:55:51 +0300

    KS-2-2-refactored

--- src/main/kotlin/Lesson_2/Task2.kt
+++ src/main/kotlin/Lesson_2/Task2.kt
@@ -2,16 +2,16 @@ package org.example.Lesson_2
 
 fun main() {
 
-        val commonWorkers = 50
-        val commonWorkersSalary = 30_000
-        val traineeWorkers = 30
-        val traineeWorkersSalary = 20_000
+    val commonWorkers = 50
+    val commonWorkersSalary = 30_000
+    val traineeWorkers = 30
+    val traineeWorkersSalary = 20_000
 
-        val constWorkersSalary = commonWorkers*commonWorkersSalary
-        val commonSalary = constWorkersSalary+traineeWorkersSalary*traineeWorkers
-        val meanSalary = commonSalary/(commonWorkers+traineeWorkers)
+    val constWorkersSalary = commonWorkers * commonWorkersSalary
+    val commonSalary = constWorkersSalary + traineeWorkersSalary * traineeWorkers
+    val meanSalary = commonSalary / (commonWorkers + traineeWorkers)
 
-        println(constWorkersSalary)
-        println(commonSalary)
-        println(meanSalary)
+    println(constWorkersSalary)
+    println(commonSalary)
+    println(meanSalary)
 }
