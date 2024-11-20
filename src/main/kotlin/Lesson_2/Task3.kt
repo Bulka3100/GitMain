9671011853d402a3861fedd53273232797d03b3c
Breaker <kirillvip3100@yandex.ru>
2024-11-20 04:19:08 +0300

    KS-2-3-train

--- src/main/kotlin/Lesson_2/Task3.kt
+++ src/main/kotlin/Lesson_2/Task3.kt
@@ -2,10 +2,10 @@ package org.example.Lesson_2
 
 fun main() {
     val startTime = 9 * 60 + 39
-    val travelTime = 457
     val arrivalTimeMinutes = startTime + travelTime
     val arrivalTimeHours = arrivalTimeMinutes / 60
     val arrivalTimeMinutesLeft = arrivalTimeMinutes % 60
     val arrivalTime: String = ("$arrivalTimeHours:$arrivalTimeMinutesLeft")
     println(arrivalTime)
 }
+const val travelTime = 457
