511bade42adba12806a5f863559dd64449aac11f
Breaker <kirillvip3100@yandex.ru>
2024-11-21 14:02:14 +0300

    KS-5-2-content

--- src/main/kotlin/Lesson_5/task2.kt
+++ src/main/kotlin/Lesson_5/task2.kt
@@ -1,5 +1,9 @@
 package org.example.Lesson_5
 
 fun main() {
-
+val birthYear = readlnOrNull()!!.toInt()
+    val age= CURRENT_YEAR-birthYear
+    if (age>=IS_ADULT) println("показать 200скрытый content") else println("подрасти")
 }
+const val CURRENT_YEAR: Short = 2024
+const val IS_ADULT: Int = 18
