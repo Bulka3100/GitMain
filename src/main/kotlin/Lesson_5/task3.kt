c875708fccec1af730bd90326d6289662021ec11
Breaker <kirillvip3100@yandex.ru>
2024-11-21 15:06:59 +0300

    KS-5-3-prize-get

--- src/main/kotlin/Lesson_5/task3.kt
+++ src/main/kotlin/Lesson_5/task3.kt
@@ -1,5 +1,25 @@
 package org.example.Lesson_5
 
 fun main() {
-
+    println("угадай число от 1 до 42")
+    val firstRight: Int = 4
+    val secondRight: Int = 22
+    val input1 = readln().toInt()
+    val input2 = readlnOrNull()!!.toInt()
+    var choosed: Int = 0
+    when (input1) {
+        4 -> choosed++
+        22 -> choosed++
+        else -> choosed
+    }
+    when (input2) {
+        4 -> choosed++
+        22 -> choosed++
+        else -> choosed
+    }
+    when (choosed) {
+        2 -> println("Вы выйграли главный приз")
+        1 -> println("Вы выйграли утешительныйф приз")
+        0 -> println("Неудача!")
+    }
 }
