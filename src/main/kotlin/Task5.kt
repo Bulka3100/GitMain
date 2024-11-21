53769bd044630ac04dad2744c024e5db83fa09a6
Breaker <kirillvip3100@yandex.ru>
2024-11-21 22:33:43 +0300

    KS-1-5-CONST

--- src/main/kotlin/Task5.kt
+++ src/main/kotlin/Task5.kt
@@ -5,8 +5,9 @@ fun main() {
     val quantityOfSeconds: Short = 6480
     val minutes = (quantityOfSeconds / QUANTITY_OF_SECONDS) - QUANTITY_OF_SECONDS
     var hours: Int = quantityOfSeconds / QUANTITY_OF_SECONDS / QUANTITY_OF_SECONDS
-    var seconds: Int = quantityOfSeconds - minutes * QUANTITY_OF_SECONDS - hours * QUANTITY_OF_SECONDS * QUANTITY_OF_SECONDS
-    println("%02d:%02d:%02d".format(hours,minutes,seconds))
+    var seconds: Int =
+        quantityOfSeconds - minutes * QUANTITY_OF_SECONDS - hours * QUANTITY_OF_SECONDS * QUANTITY_OF_SECONDS
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
 }
 
