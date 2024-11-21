492b63676a7dc9ad956e49ea90435d9af481cd9c
Breaker <kirillvip3100@yandex.ru>
2024-11-21 13:43:39 +0300

    KS-5-1-access

--- src/main/kotlin/Lesson_5/task1.kt
+++ src/main/kotlin/Lesson_5/task1.kt
@@ -1,5 +1,10 @@
 package org.example.Lesson_5
 
 fun main() {
+    val a = 2
+    val b = 3
+    println("решите пример: $a+$b")
+    val identificator = readlnOrNull()?.toInt()
+    if (identificator == a + b) println("Добро пожаловать") else println("доступ запрещен")
 
 }
