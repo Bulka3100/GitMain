f8f046af2ee63098118a453b0c181ebc1e74d681
Breaker <kirillvip3100@yandex.ru>
2024-11-22 17:31:34 +0300

    KS-2-4-Refactor

--- src/main/kotlin/Lesson_2/Task4.kt
+++ src/main/kotlin/Lesson_2/Task4.kt
@@ -3,8 +3,9 @@ package org.example.Lesson_2
 fun main() {
     val crystalOreMined = 7
     val ironOreMined = 11
-    val crystalOreMinedWithBuff = ((crystalOreMined * 1.2) - crystalOreMined).toInt()
-    val IronOreMinedWithBuff = ((ironOreMined * 1.2) - ironOreMined).toInt()
+    val Buffed = 1 + 0.20
+    val crystalOreMinedWithBuff = ((crystalOreMined * Buffed) - crystalOreMined).toInt()
+    val IronOreMinedWithBuff = ((ironOreMined * Buffed) - ironOreMined).toInt()
 
     println(crystalOreMinedWithBuff)
     println(IronOreMinedWithBuff)
