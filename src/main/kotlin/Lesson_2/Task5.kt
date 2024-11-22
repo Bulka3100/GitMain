0a42da34774d4fb569b324babb0b81660f90ce0b
Breaker <kirillvip3100@yandex.ru>
2024-11-22 17:55:40 +0300

    KS-2-5-Refactor

--- src/main/kotlin/Lesson_2/Task5.kt
+++ src/main/kotlin/Lesson_2/Task5.kt
@@ -1,5 +1,6 @@
 package org.example.Lesson_2
 
+import java.text.Format
 import kotlin.math.pow
 
 fun main() {
@@ -7,7 +8,8 @@ fun main() {
     val procent = 16.7
     val time = 20
     val result = (summ * ((1 + procent / 100).pow(time))).toDouble()
-    val formatResult = Math.round(result * 1000.0) / 1000.0
-    println(formatResult)
+    val formated = ("%.3f".format(result))
+    println(formated)
+
 
 }
