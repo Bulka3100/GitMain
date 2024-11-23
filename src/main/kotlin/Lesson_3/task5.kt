87b81375f0dd6bdac53451142ca7719bf9e800d7
Breaker <kirillvip3100@yandex.ru>
2024-11-23 17:38:53 +0300

    KS-3-5-Refactor

--- src/main/kotlin/Lesson_3/task5.kt
+++ src/main/kotlin/Lesson_3/task5.kt
@@ -1,2 +1,12 @@
 package org.example.Lesson_3
 
+fun main() {
+    val stringFormated: String = "D2-D4;0"
+    val separated = stringFormated.split("-", ";")
+    val from = separated[0]
+    val  to = separated[1]
+    val nubmerOfMove = separated[2]
+    println(from)
+    println(to)
+    println(nubmerOfMove)
+}
