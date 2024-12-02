9073dc4d9913e6e47ca7369d825fb3846f6c692e
Breaker <kirillvip3100@yandex.ru>
2024-12-02 15:29:35 +0300

    KS-6-3-seconds-update

--- src/main/kotlin/Lesson_6/task3.kt
+++ src/main/kotlin/Lesson_6/task3.kt
@@ -1,2 +1,13 @@
 package org.example.Lesson_6
 
+fun main() {
+
+    println("сколько секунд засечь?")
+    var seconds = readlnOrNull()!!.toInt()
+
+    do {
+        println("остаось секунд ${seconds--}")
+        Thread.sleep(1000)
+    } while (seconds > 0)
+    println("время вышло!")
+}
