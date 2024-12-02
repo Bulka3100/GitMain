90c1ee63d291db6298f6bb1ded949a3dfaad82f2
Breaker <kirillvip3100@yandex.ru>
2024-12-02 14:41:57 +0300

    KS-6-2-seconds

--- src/main/kotlin/Lesson_6/task2.kt
+++ src/main/kotlin/Lesson_6/task2.kt
@@ -1,2 +1,17 @@
 package org.example.Lesson_6
 
+import kotlin.concurrent.thread
+
+fun main() {
+
+    println("сколько секунд засечь?")
+    var seconds = readlnOrNull()!!.toInt()
+    var initialSeconds = seconds
+
+    do {
+        println(seconds--)
+        Thread.sleep(1000)
+    } while (seconds > 0)
+    println("прошло $initialSeconds секунд(ы)")
+
+}
