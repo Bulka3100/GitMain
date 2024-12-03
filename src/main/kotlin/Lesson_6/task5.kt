8b5f8d2ec2dec7ec1f51efb0b3da1d35fd54e5b9
Breaker <kirillvip3100@yandex.ru>
2024-12-03 14:36:27 +0300

    KS-6-5

--- src/main/kotlin/Lesson_6/task5.kt
+++ src/main/kotlin/Lesson_6/task5.kt
@@ -1,2 +1,25 @@
 package org.example.Lesson_6
 
+import kotlin.random.Random
+
+fun main() {
+    println("создайте пароль и логин")
+    var iaAutorizred = false
+    val pasword = readLine()
+    val randomNumber1 = Random.nextInt(0, 10)
+    val randomNumber2 = Random.nextInt(0, 10)
+    println("введи пароль")
+    var counter = 3
+    do {
+        val passwordEnter = readLine()
+        println("решите пример $randomNumber1+$randomNumber2")
+        var result = randomNumber1 + randomNumber2
+        do {
+           val res = readlnOrNull()?.toInt()
+            counter--
+        }while (counter>0 && result!=res)
+//        if (passwordEnter != pasword) println("неверная авторизация") else continue
+
+    } while (passwordEnter != pasword)
+    println("авторизация успешна")
+}
