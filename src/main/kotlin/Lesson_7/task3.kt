8c6d23a127d67420c04ad86be0d3799fa3b9d6e0
Breaker <kirillvip3100@yandex.ru>
2024-12-04 17:55:04 +0300

    KS-7-3-progression

--- src/main/kotlin/Lesson_7/task3.kt
+++ src/main/kotlin/Lesson_7/task3.kt
@@ -1,2 +1,9 @@
 package org.example.Lesson_7
 
+fun main() {
+    val x = readlnOrNull()!!.toInt()
+    val range = 0..x
+    for (i in range){
+        if (i%2==0) println(i) else continue
+    }
+}
