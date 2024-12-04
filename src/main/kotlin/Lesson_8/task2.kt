88929b145c9e74855ec4cd61ca94889c47caf1a9
Breaker <kirillvip3100@yandex.ru>
2024-12-04 23:46:34 +0300

    KS-8-2-ingredient

--- src/main/kotlin/Lesson_8/task2.kt
+++ src/main/kotlin/Lesson_8/task2.kt
@@ -1,2 +1,10 @@
 package org.example.Lesson_8
 
+fun main() {
+    val listOfIngredients = arrayOf("egg", "rice", "milk")
+    val ingredient = readlnOrNull()
+    for (i in listOfIngredients) {
+        if (i == ingredient) println("есть в списке") else "нет в списке"
+
+    }
+}
