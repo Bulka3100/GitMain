e5fb3da5bad6ffb2953619335d7245aba8afb618
Breaker <kirillvip3100@yandex.ru>
2024-12-05 02:16:36 +0300

    KS-8-5-ingredient-difficult

--- src/main/kotlin/Lesson_8/task5.kt
+++ src/main/kotlin/Lesson_8/task5.kt
@@ -1,2 +1,14 @@
 package org.example.Lesson_8
 
+fun main() {
+    println("сколько ингредиенттов добавить?")
+    val numberOfIngredients = readlnOrNull()!!.toInt()
+     val x = readln()
+    val y= readln()
+    var arrayOfIngredients =arrayOf<String>()
+    arrayOfIngredients+=x
+    arrayOfIngredients+=y
+    println(arrayOfIngredients.joinToString(separator = ","))
+
+}
+//доработать
