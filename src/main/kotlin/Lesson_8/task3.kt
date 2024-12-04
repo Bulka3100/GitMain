b2a9a7b1a7ff1f8b85e90f6fdb02c90f96f7bbea
Breaker <kirillvip3100@yandex.ru>
2024-12-04 23:51:52 +0300

    KS-8-3-ingredient-easy

--- src/main/kotlin/Lesson_8/task3.kt
+++ src/main/kotlin/Lesson_8/task3.kt
@@ -1,2 +1,9 @@
 package org.example.Lesson_8
 
+fun main() {
+    val listOfIngredients = arrayOf("egg", "rice", "milk")
+    val ingredient = readlnOrNull()
+    if (ingredient in listOfIngredients) println("есть в рецепте") else println("нет")
+
+
+}
