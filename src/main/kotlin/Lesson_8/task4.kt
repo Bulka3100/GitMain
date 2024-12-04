9faaef13eb73cc285f8614671ed01c381cf9038d
Breaker <kirillvip3100@yandex.ru>
2024-12-05 01:36:22 +0300

    KS-8-4-ingredient-difficult

--- src/main/kotlin/Lesson_8/task4.kt
+++ src/main/kotlin/Lesson_8/task4.kt
@@ -1,2 +1,18 @@
 package org.example.Lesson_8
 
+fun main() {
+    var arrayOfIngredients = arrayOf("лук", "чеснок", "чипсы", "паприка", "вермишель")
+    println("что заменить?")
+    val change = readlnOrNull()
+    if (change in arrayOfIngredients) {
+        println("что хотели бы добавить?")
+        val changeOn = readln()
+        arrayOfIngredients = arrayOfIngredients.filterNot { it == change }.toTypedArray()
+        arrayOfIngredients += changeOn
+        println(arrayOfIngredients.joinToString())
+    }
+    else if(change !in arrayOfIngredients){
+        println("нет такого в списке")
+    }
+    else println("ошибка")
+}
