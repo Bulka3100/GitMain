e04e8dbe448e0c54b2aa402e6c6fb6f84706a6a9
Breaker <kirillvip3100@yandex.ru>
2024-12-04 18:19:41 +0300

    KS-7-3-progression

--- src/main/kotlin/Lesson_7/task3.kt
+++ src/main/kotlin/Lesson_7/task3.kt
@@ -3,7 +3,7 @@ package org.example.Lesson_7
 fun main() {
     val x = readlnOrNull()!!.toInt()
     val range = 0..x
-    for (i in range){
-        if (i%2==0) println(i) else continue
+    for (i in range) {
+        if (i % 2 == 0) println(i) else continue
     }
 }
