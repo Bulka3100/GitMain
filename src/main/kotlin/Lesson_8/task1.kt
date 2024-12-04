c2b1efba1b5d4d24c9930d63fbd635b1038d3d14
Breaker <kirillvip3100@yandex.ru>
2024-12-04 23:23:53 +0300

    KS-8-1-Summ

--- src/main/kotlin/Lesson_8/task1.kt
+++ src/main/kotlin/Lesson_8/task1.kt
@@ -1,2 +1,21 @@
 package org.example.Lesson_8
 
+fun main() {
+    var numberOfViews = intArrayOf()
+    val monday = 20
+    val tuesday = 53
+    val wednesday = 34
+    val thursday = 43
+    val friday = 12
+    val saturday = 34
+    val sunday = 45
+    numberOfViews += monday
+    numberOfViews += tuesday
+    numberOfViews += wednesday
+    numberOfViews += thursday
+    numberOfViews += friday
+    numberOfViews += saturday
+    numberOfViews += sunday
+    println(numberOfViews.sum())
+
+}
