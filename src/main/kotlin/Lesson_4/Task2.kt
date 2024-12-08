020ad8d142bb0cdd97d037eead5bcd956457fd0d
Breaker <kirillvip3100@yandex.ru>
2024-12-08 19:47:32 +0300

    KS-4-2-REFACTOR

--- src/main/kotlin/Lesson_4/Task2.kt
+++ src/main/kotlin/Lesson_4/Task2.kt
@@ -5,6 +5,11 @@ fun main() {
     val good1Volume = 80
     val good2Weight = 50
     val good2Volume = 100
-    println("Груз весом 20кг и объёмом 80 л соответствует категории 'Avverage':${(good1Weight > 35 && good1Weight <= 100) && good1Volume < 100}")
-    println("Груз весом 20кг и объёмом 80 л соответствует категории 'Avverage':${(good2Weight > 35 && good2Weight <= 100) && good2Volume < 100}")
+    val message1 = "Груз весом 20кг и объёмом 80 л соответствует категории 'Avverage'"
+    val message2 = "Груз весом 50кг и объёмом 100 л соответствует категории 'Avverage'"
+
+    println("$message1:${(good1Weight > WEIGHT_RESTRICTION && good1Weight <= WEIGHT_RESTRICTION) && good1Volume < VOLEME_RESTRICTION}")
+    println("$message2:${(good2Weight > WEIGHT_RESTRICTION && good2Weight <= VOLEME_RESTRICTION) && good2Volume < VOLEME_RESTRICTION}")
 }
+const val WEIGHT_RESTRICTION = 35
+const val VOLEME_RESTRICTION = 100
