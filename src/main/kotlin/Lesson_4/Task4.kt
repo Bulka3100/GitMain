470233f3331aa0522e6f33da092a9280e454af74
Breaker <kirillvip3100@yandex.ru>
2024-12-10 04:44:12 +0300

    KS-4-4-REFACTOR

--- src/main/kotlin/Lesson_4/Task4.kt
+++ src/main/kotlin/Lesson_4/Task4.kt
@@ -2,12 +2,12 @@ package org.example.Lesson_4
 
 fun main() {
     val day = 5
-    val isArmsAndAbsDay = day % 2 == 1
+    val isArmsAndAbsDay = day % 2 == 0
     val output = """
-        Упражнения для рук:       $isArmsAndAbsDay
-        Упражнения для ног:       ${!isArmsAndAbsDay}
-        Упражнения для спины:     ${!isArmsAndAbsDay}
-        Упражнения для пресса:    $isArmsAndAbsDay
+        Упражнения для рук:      ${!isArmsAndAbsDay}
+        Упражнения для ног:      $isArmsAndAbsDay
+        Упражнения для спины:    $isArmsAndAbsDay
+        Упражнения для пресса:   ${!isArmsAndAbsDay}
     """.trimIndent()
 
     println(output)
