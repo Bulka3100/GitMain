44d2482f895158d4a61156155f133ab51ca9c6b2
Breaker <kirillvip3100@yandex.ru>
2024-12-10 04:52:04 +0300

    KS-4-5-REFACTOR

--- src/main/kotlin/Lesson_4/Task5.kt
+++ src/main/kotlin/Lesson_4/Task5.kt
@@ -3,10 +3,13 @@ package org.example.Lesson_4
 fun main() {
     fun main() {
         val isInjured: Boolean = readln().toBoolean()
-        val isPeople = readlnOrNull()!!.toInt()
+        val isPeople = readln().toInt()
         val enoughBoxes = readlnOrNull()!!.toInt()
         val weatherIsFine: Boolean = readln().toBoolean()
-        val shipIsReady = isInjured == true && isPeople in 55..70 && enoughBoxes > 50 && weatherIsFine == true
+        val shipIsReady = isInjured == true && isPeople in RECOMMENDED_PEOPLE_FROM..RECOMMENDED_PEOPLE_TO && enoughBoxes > BOXES_NEEDED && weatherIsFine == true
         println("Готовность коробля: $shipIsReady")
     }
 }
+const val BOXES_NEEDED = 50
+const val RECOMMENDED_PEOPLE_TO = 70
+const val RECOMMENDED_PEOPLE_FROM = 55
