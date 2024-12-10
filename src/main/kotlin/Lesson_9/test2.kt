64f8f25c5b32a4424acf964624bda71f946a3c2b
Breaker <kirillvip3100@yandex.ru>
2024-12-11 00:16:24 +0300

    KS-9-2-ingredients-new

--- src/main/kotlin/Lesson_9/test2.kt
+++ src/main/kotlin/Lesson_9/test2.kt
@@ -1,2 +1,17 @@
 package org.example.Lesson_9
 
+fun main() {
+    val ingredients = mutableListOf<String>("лук", "морковь", "сгущенка")
+    println("в рецепте есть базовые ингредиенты:$ingredients")
+    println("желаете добавить ещё?")
+    val chosen = readln()
+    if (chosen.equals("нет", ignoreCase = true)) {
+
+    } else if (chosen.equals("да", ignoreCase = true)) {
+        println("какой ингридиент добавить?")
+        val ingredientNew = readln()
+        ingredients.add(ingredientNew)
+        println("в рецепте теперь есть ингредиенты:$ingredients")
+    } else {
+    }
+}
