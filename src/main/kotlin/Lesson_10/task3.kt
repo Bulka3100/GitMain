c87e83ba16aca841e98d020c6971e5a53a72977c
Breaker <kirillvip3100@yandex.ru>
2024-12-11 22:55:22 +0300

    KS-10-3-generating-password

--- src/main/kotlin/Lesson_10/task3.kt
+++ src/main/kotlin/Lesson_10/task3.kt
@@ -1,2 +1,24 @@
 package org.example.Lesson_10
 
+fun main() {
+val a =generetePass(90)
+    println(a)
+}
+
+fun generetePass(numberOfSymbols: Int): String {
+    var result = ""
+    for (i in 1..numberOfSymbols) {
+        if (i % 2 == 0) result += addSymbol() else result += addInt()
+    }
+    return result
+}
+
+fun addInt(): String {
+    val arrayInt = 0..9
+    return arrayInt.random().toString()
+}
+
+fun addSymbol(): String {
+    val special = listOf<String>("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "")
+    return special.random()
+}
