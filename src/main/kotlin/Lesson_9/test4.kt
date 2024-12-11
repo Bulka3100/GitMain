fe19c002396975228c7931a7bc32abc4840d9f45
Breaker <kirillvip3100@yandex.ru>
2024-12-11 04:00:23 +0300

    KS-9-4-ingredients-with-question

--- src/main/kotlin/Lesson_9/test4.kt
+++ src/main/kotlin/Lesson_9/test4.kt
@@ -1,2 +1,8 @@
 package org.example.Lesson_9
 
+fun main() {
+  val ingredients=readln()
+   val input = ingredients.split(",")
+    val sorted = input.sorted()
+    println("отсортированный список: $sorted")
+}
