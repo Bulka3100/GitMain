c6c634b8c828f453104e7f90cc05ca895b0dad39
Breaker <kirillvip3100@yandex.ru>
2024-12-11 03:40:39 +0300

    KS-9-3-ingredients-index

--- src/main/kotlin/Lesson_9/test3.kt
+++ src/main/kotlin/Lesson_9/test3.kt
@@ -1,2 +1,12 @@
 package org.example.Lesson_9
 
+fun main() {
+    val ingredients = listOf<String>("Яица", "молоко", "сливочное масло")
+    val quantityOfIngredients = listOf<Int>(2, 50, 15)
+    println("сколько порций?")
+    val choice = readlnOrNull()!!.toInt()
+    println("на $choice порций нужно")
+    for ((i, ingredient) in ingredients.withIndex()) {
+        println("$ingredient-${quantityOfIngredients[i] * choice}")
+    }
+}
