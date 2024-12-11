280919ef98bcbd5d4196fcd8dbc37e250a21dd29
Breaker <kirillvip3100@yandex.ru>
2024-12-11 19:04:44 +0300

    package-10

--- src/main/kotlin/Lesson_10/task1.kt
+++ src/main/kotlin/Lesson_10/task1.kt
@@ -1,10 +1,20 @@
 package org.example.Lesson_10
 
 fun main() {
-
+    val firstRobot = throwBone()
+    println("машина: $firstRobot")
+    val secondHuman = throwBone()
+    println("Человек: $secondHuman")
+    if (firstRobot > secondHuman) println("Победила машина")
+    else if (firstRobot < secondHuman) println("Победило человечество")
+    else println("Победила дружба")
 }
-fun throwBone: Int (){
+
+fun throwBone(): Int {
     println("бросок кости")
-    val randomThrow = random.nextInt()
-
+    val dust = 1..6
+    val randomThrow = dust.random()
+    return randomThrow
 }
+//есть ли лучше варианты создать рандом
+
