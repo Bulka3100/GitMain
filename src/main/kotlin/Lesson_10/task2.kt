45c0a280ac0913bffcbc223f570da5cce3f3473a
Breaker <kirillvip3100@yandex.ru>
2024-12-11 21:12:27 +0300

    KS-10-2-fun-log

--- src/main/kotlin/Lesson_10/task2.kt
+++ src/main/kotlin/Lesson_10/task2.kt
@@ -1,2 +1,33 @@
 package org.example.Lesson_10
 
+fun main() {
+    println("введите логин")
+    val login = gettt()
+
+    println("введите пароль")
+    val password = gettt()
+}
+
+/*fun valid(inputLength: String): Boolean {
+    return inputLength.length > 4
+}
+*/
+/*fun getValidInput(): String {
+    var input: String
+    do {
+        input = readln()
+        if (!valid(input)) {
+            println("Логин или пароль недостаточно длинные. Повторите попытку.")
+        }
+    } while (!valid(input))
+    return input
+}*/
+
+fun gettt(): String {
+    var login: String
+    do {
+        login = readln()
+        if(login.length<4) println("меньше 4х символов")
+    } while (login.length < 4)
+    return login
+}
