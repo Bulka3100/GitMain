1db3d7e188140bb2e43a3973832fccad19a1375f
Breaker <kirillvip3100@yandex.ru>
2024-12-13 06:18:26 +0300

    KS-11-2

--- src/main/kotlin/Lesson_11/task2.kt
+++ src/main/kotlin/Lesson_11/task2.kt
@@ -1,2 +1,25 @@
 package org.example.Lesson_11
 
+fun main() {
+
+}
+
+class User2(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+    val bio: String = "",
+) {
+    fun userData(){
+        println()
+    }
+    fun changePass() {
+        println("введите текущий пароль")
+        val ininialPass = readln()
+        if (ininialPass == password) {
+            var newPass = readln()
+            newPass = password
+        }
+    }
+}
