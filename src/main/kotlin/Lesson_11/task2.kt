0780882e73d5314ce5a66939cc8d67a264013b0d
Breaker <kirillvip3100@yandex.ru>
2024-12-13 06:38:13 +0300

    KS-11-2-funk

--- src/main/kotlin/Lesson_11/task2.kt
+++ src/main/kotlin/Lesson_11/task2.kt
@@ -1,25 +1,56 @@
 package org.example.Lesson_11
 
-fun main() {
+import kotlin.String
 
+fun main() {
+    val userPetr = User2(
+        id = 1,
+        login = "pasha",
+        password = "jjnjj22",
+        mail = "apasha65@mail.ru",
+    )
+
+    userPetr.changeBio()
+    println(userPetr.bio)
+    println(userPetr.password)
+    userPetr.changePass()
+    println(userPetr.password)
+    userPetr.userData()
 }
 
 class User2(
     val id: Int,
     val login: String,
-    val password: String,
+    var password: String,
     val mail: String,
-    val bio: String = "",
+    var bio: String = "",
 ) {
-    fun userData(){
-        println()
+    fun userData() {
+        println(
+            """
+         $id
+         $login
+         $password
+         $mail
+         $bio
+    """.trimIndent()
+        )
     }
+
+    fun changeBio() {
+        println("введи информацию о себе")
+        val bioInf = readln()
+        bio = bioInf
+
+    }
+
     fun changePass() {
         println("введите текущий пароль")
         val ininialPass = readln()
         if (ininialPass == password) {
+            println("новый пароль:")
             var newPass = readln()
-            newPass = password
+             password = newPass
         }
     }
 }
