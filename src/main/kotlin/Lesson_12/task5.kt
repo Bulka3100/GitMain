3d8313c31347c05e34ac340a24ee1bd73163bf2f
Breaker <kirillvip3100@yandex.ru>
2024-12-14 23:52:45 +0300

    KS-12-5-need help

--- src/main/kotlin/Lesson_12/task5.kt
+++ src/main/kotlin/Lesson_12/task5.kt
@@ -1,29 +1,38 @@
 package org.example.Lesson_12
 
+import kotlin.random.Random
 
 
 fun main() {
-    val list = listOf<Int>()
+    val weatherList = mutableListOf<Weather>()
 
+    for (i in 1..30) {
+        val dayTemp: Int = Random.nextInt(10, 40)
+        val nightTemp: Int = Random.nextInt(8, 30)
+        val isRain: Boolean = Random.nextBoolean()
+        weatherList.add(Weather(dayTemp, nightTemp, isRain))
+    }
+    val night = weatherList.map { it.dayTemp }
+    val day = weatherList.map { it.nightTemp }
+    val rain = weatherList.count{it.isRain}
+    println(night)
+    println(day)
 }
-val November15 = Weather(
-    _dayTemp = 13,
-    _nightTemp = 11,
-)
 
+fun Iterable<Int>.calculateAverage(): Double {
+    if (isEmpty()) return 0.0
+    return sum().toDouble() / size
 }
-class random(
-    val random =random(10..40)
-)
+
 class Weather(
     _dayTemp: Int,
     _nightTemp: Int,
+    _isRain: Boolean,
 ) {
-    var dayTemp=_dayTemp
-    var nightTemp=_nightTemp
-    init {
-        println("днём $dayTemp вечером $nightTemp")
-    }
+    var dayTemp = _dayTemp
+    var nightTemp = _nightTemp
+    var isRain = _isRain
+
     fun showAll() {
         println("днём $dayTemp вечером $nightTemp")
 
