0d10bb53c9951f20ec5fd956f0cc3dca54767474
Breaker <kirillvip3100@yandex.ru>
2024-12-14 06:10:23 +0300

    KS-11-4

--- src/main/kotlin/Lesson_11/task4.kt
+++ src/main/kotlin/Lesson_11/task4.kt
@@ -1,2 +1,35 @@
 package org.example.Lesson_11
 
+
+fun main() {
+
+}
+
+class Recipe(
+    val recipeName: String,
+    val id: Int,
+    var inFavorites: Boolean = false,
+    val recipePhoto: String = "default.png",
+    val description: String = ""
+) {
+    fun addToFavorites() {
+        inFavorites = true
+    }
+}
+
+
+class RecipeCategory(
+    val categoryName: List<String> = listOf<String>("Бургеры", "Десерты", "Пицца", "Рыба"),
+    val categoryPhoto: String = "default.png"
+)
+
+class Ingredient(
+    val ingId: Int,
+    val ingName: String,
+    val ingState: List<String> = listOf<String>("solid", "liquid")
+)
+
+fun forPortions(numberOfPortions: Int, ingQuantity: Int): Int {
+    val result = numberOfPortions * ingQuantity
+    return result
+}
