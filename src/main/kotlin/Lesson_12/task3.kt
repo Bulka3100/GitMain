79061f04cb3e40ab0aa24d5b8d43c6262675bf77
Breaker <kirillvip3100@yandex.ru>
2024-12-14 08:10:37 +0300

    KS-12-3-weather-constructor-no-memory

--- src/main/kotlin/Lesson_12/task3.kt
+++ src/main/kotlin/Lesson_12/task3.kt
@@ -1,3 +1,22 @@
 package org.example.Lesson_12
 
-fun main() {}
+fun main() {
+        val November15 = Weather(
+            _dayTemp = 13,
+            _nightTemp = 11,
+        )
+        November15.showAll()
+    }
+
+    class Weather(
+        _dayTemp: Int,
+        _nightTemp: Int,
+    ) {
+        var dayTemp=_dayTemp
+        var nightTemp=_nightTemp
+        fun showAll() {
+         println("днём $dayTemp вечером $nightTemp")
+        }
+    }//я правильно понял что именно так я не занимаю память в конструкторе?
+//я правильно понял что само свойство у нас dayTemp а _dayTemp это всего лишь обращение через него в конструкторе
+// свойство и поле это разные вещи?
