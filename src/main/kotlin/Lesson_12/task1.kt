5f116722194d41af6879fdb7848430b7df05b397
Breaker <kirillvip3100@yandex.ru>
2024-12-14 07:46:12 +0300

    KS-12-1-weather

--- src/main/kotlin/Lesson_12/task1.kt
+++ src/main/kotlin/Lesson_12/task1.kt
@@ -1,3 +1,21 @@
 package org.example.Lesson_12
 
-fun main() {}
+fun main() {
+    val November12 = Weatherdata()
+    November12.dayTemp = 15
+    November12.isRainfall = true
+    November12.showAll()
+
+    val November14 = Weatherdata()
+    November14.showAll()
+}
+
+
+class Weatherdata() {
+    var dayTemp = 22
+    var nighttemp = 17
+    var isRainfall = false
+    fun showAll() {
+        println("днём $dayTemp вечером $nighttemp осадки:$isRainfall")
+    }
+    }
