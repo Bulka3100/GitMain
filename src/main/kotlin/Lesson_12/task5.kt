75a31c492eb7e1360888b2e9f1e206853dde0f8e
Breaker <kirillvip3100@yandex.ru>
2024-12-14 21:50:15 +0300

    KS-12-5-init

--- src/main/kotlin/Lesson_12/task5.kt
+++ src/main/kotlin/Lesson_12/task5.kt
@@ -1,3 +1,31 @@
 package org.example.Lesson_12
 
-fun main() {}
+
+
+fun main() {
+    val list = listOf<Int>()
+
+}
+val November15 = Weather(
+    _dayTemp = 13,
+    _nightTemp = 11,
+)
+
+}
+class random(
+    val random =random(10..40)
+)
+class Weather(
+    _dayTemp: Int,
+    _nightTemp: Int,
+) {
+    var dayTemp=_dayTemp
+    var nightTemp=_nightTemp
+    init {
+        println("днём $dayTemp вечером $nightTemp")
+    }
+    fun showAll() {
+        println("днём $dayTemp вечером $nightTemp")
+
+    }
+}//в блоке инит у нас происходит любой код что мы напишем и он выполняется когда мы создаем объект?
