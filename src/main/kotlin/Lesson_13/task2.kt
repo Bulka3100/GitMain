228028ce290caf0bcecd99b85cd0bb50fc3fe30f
Breaker <kirillvip3100@yandex.ru>
2024-12-15 07:12:54 +0300

    KS-13-2

--- src/main/kotlin/Lesson_13/task2.kt
+++ src/main/kotlin/Lesson_13/task2.kt
@@ -1,2 +1,19 @@
 package org.example.Lesson_13
 
+fun main() {
+val client1= Client(
+    name="Ростик",
+    number = 88005553535,
+    company = "VK"
+)
+    client1.showInfo()
+}
+class Client(
+    val name: String,
+    val number: Long,
+    val company: String?=null ,
+){
+    fun showInfo(){
+        println("имя:$name \nномер:$number \nкомпания:$company")
+    }
+}
