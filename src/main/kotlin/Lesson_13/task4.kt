b8251dbb012484d899d89074957da026887164fc
Breaker <kirillvip3100@yandex.ru>
2024-12-15 09:17:27 +0300

    KS-13-4-question

--- src/main/kotlin/Lesson_13/task4.kt
+++ src/main/kotlin/Lesson_13/task4.kt
@@ -1,2 +1,29 @@
 package org.example.Lesson_13
 
+fun main() {
+    val сlientlist = mutableListOf<Client>()
+    println("введите номер телефона")
+    val phoneNumber = readln().toLongOrNull()
+    if (phoneNumber != null) {
+        val correctNumber = phoneNumber // почему тип long? по умолчанию если я ввел проверку что это не нулл
+        println("введите название компании ")
+        val companyName: String? = readln()
+        println("введите имя")
+        val name: String = readln() //можно самому проставить что readln возвращает ?
+
+        val client1 = Client(name,correctNumber,companyName)
+        сlientlist.add(client1)
+    } else println("вы не ввели номер")
+
+
+}
+
+class Client(
+    val name: String,
+    val number: Long,
+    val company: String? = null ?: "не указано",
+) {
+    fun showInfo() {
+        println("имя:$name \nномер:$number \nкомпания:$company")
+    }
+}
