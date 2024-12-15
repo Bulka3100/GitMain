b0385dd5da951c2eec47c24b36241f4622644fbb
Breaker <kirillvip3100@yandex.ru>
2024-12-15 07:15:12 +0300

    KS-13-2-elwis

--- src/main/kotlin/Lesson_13/task2.kt
+++ src/main/kotlin/Lesson_13/task2.kt
@@ -1,19 +1,20 @@
 package org.example.Lesson_13
 
 fun main() {
-val client1= Client(
-    name="Ростик",
-    number = 88005553535,
-    company = "VK"
-)
+    val client1 = Client(
+        name = "Ростик",
+        number = 88005553535,
+        company = "VK"
+    )
     client1.showInfo()
 }
+
 class Client(
     val name: String,
     val number: Long,
-    val company: String?=null ,
-){
-    fun showInfo(){
+    val company: String? = null ?: "не указано",
+) {
+    fun showInfo() {
         println("имя:$name \nномер:$number \nкомпания:$company")
     }
 }
