a7fef1ed78ffad3b5374453aee508bacbdf5ef2f
Breaker <kirillvip3100@yandex.ru>
2024-12-15 06:54:01 +0300

    KS-13-1

--- src/main/kotlin/Lesson_13/task1.kt
+++ src/main/kotlin/Lesson_13/task1.kt
@@ -1,2 +1,9 @@
 package org.example.Lesson_13
 
+fun main() {
+}
+class ClientData(
+    val name: String,
+    val number: Long,
+    val company: String?=null ,//но ведь это инициализация по умолчанию?
+)
