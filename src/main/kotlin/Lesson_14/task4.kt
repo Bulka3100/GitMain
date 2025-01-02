cfc468a6e3480e85bea546b4f41a1c059e7c5b70
Breaker <kirillvip3100@yandex.ru>
2025-01-03 00:17:36 +0300

    KS-14-4-planets

--- src/main/kotlin/Lesson_14/task4.kt
+++ src/main/kotlin/Lesson_14/task4.kt
@@ -1,2 +1,32 @@
 package org.example.Lesson_14
 
+fun main() {
+    val moon = Satellite("moon", true, true)
+    val bigR = Satellite("Rbig", false, true)
+    val earth = Planet("Earth", true, true, listOf(bigR, moon))
+    println(earth.name)
+    println(earth.showSatellites())
+}
+
+open class SkyObjects(
+    val isAutmosphere: Boolean,
+    val isSafeForLanding: Boolean
+)
+
+class Planet(
+    val name: String,
+    isAutmosphere: Boolean,
+    isSafeForLanding: Boolean,
+    val satellites: List<Satellite> = listOf<Satellite>(),
+) : SkyObjects(isAutmosphere, isSafeForLanding) {
+    fun showSatellites() {
+        println("Спутники:")
+        satellites.forEach { println(it.name) }
+    }
+}
+
+class Satellite(
+    val name: String,
+    isAutmosphere: Boolean,
+    isSafeForLanding: Boolean
+) : SkyObjects(isAutmosphere, isSafeForLanding)
