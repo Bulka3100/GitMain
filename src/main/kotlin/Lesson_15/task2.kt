a72434b757076704fcf6231260e6b4402453da8d
Breaker <kirillvip3100@yandex.ru>
2025-01-03 08:36:04 +0300

    KS-15-2-weather

--- src/main/kotlin/Lesson_15/task2.kt
+++ src/main/kotlin/Lesson_15/task2.kt
@@ -1,5 +1,43 @@
 package org.example.Lesson_15
 
-fun main() {
+import sun.awt.www.content.audio.wav
 
+fun main() {
+    val temp = Temperature(12.5)
+    val preci = PrecipitationAmount(43.4)
+    val weatherServer = WeatherServer()
+    weatherServer.sentData(temp)
+    weatherServer.sentData(preci)
+}
+
+abstract class WeatherStationStats(
+
+) {
+
+}
+
+class Temperature(
+    val temp: Double
+) : WeatherStationStats() {
+    override fun toString(): String {
+        return temp.toString()
+    }
+}
+
+class PrecipitationAmount(
+    val preci: Double
+) : WeatherStationStats() {
+    override fun toString(): String {
+        return preci.toString()
+    }
+}
+
+class WeatherServer {
+    fun sentData(data: WeatherStationStats) {
+        if (data is Temperature) {
+            println("температура на сегодня $data")
+        } else if (data is PrecipitationAmount) {
+            println("количчество  осадков на сегодня $data")
+        }
+    }
 }
