2939bc7f9d156b352a8d6731e479e28451d585f5
Breaker <kirillvip3100@yandex.ru>
2025-01-03 07:36:45 +0300

    KS-15-1-Movement

--- src/main/kotlin/Lesson_15/task1.kt
+++ src/main/kotlin/Lesson_15/task1.kt
@@ -1,2 +1,29 @@
 package org.example.Lesson_15
 
+fun main() {
+val seagull= Seagull()
+    val fish= Fish()
+    val duck= Duck()
+    seagull.toMove()
+    duck.toMove()
+    fish.toMove()
+}
+class Fish() : Movable {
+    override fun toMove() {
+        println("плывёт")
+    }
+}
+class Duck(): Movable{
+    override fun toMove() {
+        println("плывёт и ходит")
+    }
+}
+class Seagull(): Movable{
+    override fun toMove() {
+        println("ходит")
+    }
+}
+
+interface Movable{
+    fun toMove()
+}
