1da21c829ac4c0faaf81840f6ba41b196fe21b6f
Breaker <kirillvip3100@yandex.ru>
2025-01-03 21:34:01 +0300

    KS-15-3-usera

--- src/main/kotlin/Lesson_15/task3.kt
+++ src/main/kotlin/Lesson_15/task3.kt
@@ -1,5 +1,39 @@
 package org.example.Lesson_15
 
 fun main() {
+    val regularUser = RegularUser("Dopper")
+    regularUser.readForum()
+    val administrator = Administrator("MODERATOR")
+    administrator.banUser(regularUser)
+}
 
+abstract class User(
+    val userName: String,
+    val administrationRights: Boolean,
+) {
+    fun readForum() {
+        println("статья открыта")
+    }
+
+    fun comment() {
+        println("сообщение оставлено")
+    }
+}
+
+class RegularUser(
+    userName: String,
+    administrationRights: Boolean = false
+) : User(userName, administrationRights)
+
+class Administrator(
+    userName: String,
+    administrationRights: Boolean = true
+) : User(userName, administrationRights) {
+    fun banUser(user: RegularUser) {
+        println("пользователь ${user.userName} заблокирован")
+    }
+
+    fun deleteComment() {
+println("сообщение удалено")
+    }
 }
