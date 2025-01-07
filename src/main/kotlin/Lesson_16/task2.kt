255d109222a19fe9a9c7f0d20458cca270ec792a
Breaker <kirillvip3100@yandex.ru>
2025-01-07 06:55:00 +0300

    KS-16-2-Calculating-Circle

--- src/main/kotlin/Lesson_16/task2.kt
+++ src/main/kotlin/Lesson_16/task2.kt
@@ -1,5 +1,26 @@
 package org.example.Lesson_16
 
 fun main() {
+    val circle = Circle(4)
+    println(Circle(2).calcP())
+    println(circle.calcS())
 
 }
+
+class Circle(
+    private val radius: Int
+) {
+    fun calcP(): Double {
+        return P * 2 * radius
+    }
+
+    fun calcS(): Double {
+        return P * radius * radius
+    }
+}
+
+const val P = 3.14
+
+/*fun calcS(radius: Int): Double {
+return P * radius * radius А если я так пишу то получается когда я передаю в функцию хначение радиуса он меняется в самом конструкторе? А как тае если он val? Ошибку не выдвет
+}*/
