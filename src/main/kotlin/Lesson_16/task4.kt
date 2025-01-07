c6bfb9ecfa9f742a020b15cea538e10d920623f1
Breaker <kirillvip3100@yandex.ru>
2025-01-07 07:35:16 +0300

    KS-16-4-orders

--- src/main/kotlin/Lesson_16/task4.kt
+++ src/main/kotlin/Lesson_16/task4.kt
@@ -1,5 +1,32 @@
 package org.example.Lesson_16
 
 fun main() {
+    val order1 = InternetOrder(1, false)
+    order1.showStatus()
+    sendRequest(order1, true)
+    order1.showStatus()
+
+}
+
+class InternetOrder(
+    private val id: Int,
+    private var status: Boolean,
+) {
+    fun changeStatus(newValue: Boolean) {
+        status = newValue
+    }
+
+    fun showStatus() {
+        println(status)
+    }
+    fun returnId(): Int{
+        return id
+    }
+
+}
+
+fun sendRequest(order: InternetOrder, newStatus: Boolean) {
+    println("Поменяйте пожалуйста статус заказа ${order.returnId()}")
+    order.changeStatus(newStatus)
 
 }
