74097306be22945c20664767fb5d8256a0bbed5a
Breaker <kirillvip3100@yandex.ru>
2025-01-07 07:12:04 +0300

    KS-16-3

--- src/main/kotlin/Lesson_16/task3.kt
+++ src/main/kotlin/Lesson_16/task3.kt
@@ -1,5 +1,18 @@
 package org.example.Lesson_16
 
 fun main() {
+    val user1 = User("loshka", "Bulorg")
+    user1.validation("loshka")
+}
 
+class User(
+    private val password: String,
+    val login: String,
+) {
+    fun validation(input: String): Boolean {
+        if (input == password) {
+            println("верно")
+            return true
+        } else return false
+    }
 }
