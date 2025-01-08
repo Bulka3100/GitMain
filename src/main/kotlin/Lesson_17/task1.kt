8f04a2017878eef7bfecdf7904d518ecefaf6524
Breaker <kirillvip3100@yandex.ru>
2025-01-08 13:24:26 +0300

    KS-17-1-Victorina

--- src/main/kotlin/Lesson_17/task1.kt
+++ src/main/kotlin/Lesson_17/task1.kt
@@ -1,2 +1,21 @@
 package org.example.Lesson_17
 
+fun main() {
+val victorina= Victorina("сколько?","немало")
+    println(victorina._question)
+    println(victorina._answer) //я думал что у меня свойство будет question без нижнего подчеркивания из конструктора, почему оно все таки не то?
+
+}
+class Victorina(question: String,answer: String){
+    val _question=question
+        get()=field
+    var _answer=answer
+        get()=field
+        set(value){
+            field=value
+        }
+}
+
+// у меня проблема с тем как мне использовать геттер и сеттер при условии того, что у меня свойства определенны в первичном конструкторе. Как я понял удобно их определять когда свойства прописаны в теле класса
+
+
