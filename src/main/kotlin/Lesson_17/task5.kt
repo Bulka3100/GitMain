82311bf1f40906c4f0f486ed391392bab9ea1b64
Breaker <kirillvip3100@yandex.ru>
2025-01-08 16:37:24 +0300

    KS-17-5-password

--- src/main/kotlin/Lesson_17/task5.kt
+++ src/main/kotlin/Lesson_17/task5.kt
@@ -1,2 +1,24 @@
 package org.example.Lesson_17
 
+fun main() {
+    val user = User("Boilix", "strew3901")
+    user.login = "Kronos"
+    println(user.password)
+
+}
+
+
+class User(login: String, password: String) {
+    var login = login
+        get() = field
+        set(value) {
+            println("логин успешно сменён")
+            field = value
+        }
+    var password = password
+        get() = field.map { '*' }.joinToString("")
+        set(value) {
+            if (value != field)
+                println("нельзя изменить пароль")
+        }
+}
