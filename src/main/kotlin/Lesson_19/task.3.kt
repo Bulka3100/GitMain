7cf5e3f5ba72f8265c19a173330d7c2b93413308
Breaker <kirillvip3100@yandex.ru>
2025-01-18 22:27:53 +0300

    KS-19-3-todo

--- src/main/kotlin/Lesson_19/task.3.kt
+++ src/main/kotlin/Lesson_19/task.3.kt
@@ -1,2 +1,26 @@
 package org.example.Lesson_19
 
+fun main() {
+    val spaceShip = SpaceShip("shatle", 120)
+    spaceShip.toShoot()
+    spaceShip.toLand()
+    spaceShip.toFly()
+}
+
+class SpaceShip(
+    val name: String,
+    val speed: Int,
+
+    ) {
+    fun toFly() {
+        //TODO ("описать логику")
+    }
+
+    fun toLand() {
+        TODO(" тут логика нужна")
+    }
+
+    fun toShoot() {
+        println("выстрел в астеройды")
+    }
+}
