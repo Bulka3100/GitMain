265bb3f3061a61e6362f9d2b9404a456fde0f339
Breaker <kirillvip3100@yandex.ru>
2025-01-18 22:54:34 +0300

    KS-19-4-tank

--- src/main/kotlin/Lesson_19/task.4.kt
+++ src/main/kotlin/Lesson_19/task.4.kt
@@ -1,2 +1,41 @@
 package org.example.Lesson_19
 
+fun main() {
+    val tank = Tank()
+    tank.shoot()
+    tank.getNewAmmmo(Ammo.RED)
+    tank.shoot()
+    tank.getNewAmmmo(Ammo.GREEN)
+    tank.shoot()
+
+}
+
+class Tank(
+    var ammo: Ammo = Ammo.NO_AMMO
+) {
+    fun getNewAmmmo(newAmmo: Ammo) {
+        ammo = newAmmo
+    }
+
+    fun shoot() {
+        when (ammo) {
+            Ammo.BLUE -> println("нанесено $blueDamage урона")
+            Ammo.GREEN -> println("нанесено $greenDamage урона")
+            Ammo.RED -> println("нанесено $redDamage урона")
+            else -> "нет патронов"
+        }
+    }
+
+    companion object {
+        val blueDamage = 5
+        val redDamage = 20
+        val greenDamage = 10
+    }
+}
+
+enum class Ammo {
+    BLUE,
+    GREEN,
+    RED,
+    NO_AMMO,
+}
