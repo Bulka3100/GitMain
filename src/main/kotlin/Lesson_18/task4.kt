b34260ba13dbc5f18252cbd2b8f39a5232c395c0
Breaker <kirillvip3100@yandex.ru>
2025-01-18 18:08:12 +0300

    KS-18-4-Find-S

--- src/main/kotlin/Lesson_18/task4.kt
+++ src/main/kotlin/Lesson_18/task4.kt
@@ -1,2 +1,30 @@
 package org.example.Lesson_18
 
+fun main() {
+    val cube= Cube(3)
+    val rectangle= Rectangle(2,3,6)
+    println(cube.findS())
+    println(rectangle.findS())
+}
+
+abstract class Package() {
+    abstract fun findS(): Int
+}
+
+class Rectangle(
+    val height:Int,
+    val width:Int,
+    val length: Int,
+): Package() {
+    override fun findS(): Int {
+        return 2*(height*width+width*length+height*length)
+    }
+
+}
+class Cube(
+    val edgeLength: Int
+): Package() {
+    override fun findS(): Int {
+        return 6*edgeLength*edgeLength
+    }
+}
