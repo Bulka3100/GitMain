afbfbb3aae4c51be12a5e6f2264055bb2b0c65fb
Breaker <kirillvip3100@yandex.ru>
2025-01-18 23:16:18 +0300

    KS-19-5

--- src/main/kotlin/Lesson_19/task.5.kt
+++ src/main/kotlin/Lesson_19/task.5.kt
@@ -1,2 +1,25 @@
 package org.example.Lesson_19
 
+fun main(){
+    println("Добро пожаловать пользователь. Вбей сначала имя, затем пол")
+    val humanList= mutableListOf<Human>()
+    val name= readln()
+    val sex: Sex=readln()
+    val human1= Human(name,sex)
+    val human2= Human(name,sex)
+    val human3= Human(name,sex)
+    val human4=Human(name,sex)
+    val human5=Human(name,sex)
+    humanList.add(human5)
+
+}
+
+class Human(
+    name: String,
+    sex: Sex,
+)
+enum class Sex{
+    MALE,
+    FEMALE,
+    ANOTHER,
+}
