be8ad490a081e76adf61493202f3ebaf24defcff
Breaker <kirillvip3100@yandex.ru>
2025-01-18 22:00:31 +0300

    KS-19-2-clothes

--- src/main/kotlin/Lesson_19/task.2.kt
+++ src/main/kotlin/Lesson_19/task.2.kt
@@ -1,2 +1,38 @@
 package org.example.Lesson_19
 
+fun main() {
+    val product = Product("shirt", 2, Category.CLOTHES)
+    product.getInfo()
+}
+
+enum class Category() {
+    CLOTHES {
+        override fun getDescription(): String {
+            return "Одежда любых размеров"
+        }
+
+    },
+    DIFFERENT {
+        override fun getDescription(): String {
+            return "От книг до товаров для дома"
+        }
+    },
+    CHANCELLERY {
+        override fun getDescription(): String {
+            return "пишущие принадлежности"
+        }
+    };
+
+    abstract fun getDescription(): String
+
+}
+
+class Product(
+    val name: String,
+    val id: Int,
+    val category: Category
+) {
+    fun getInfo() {
+        println("название $name, айди $id, категория ${category.getDescription()}")
+    }
+}
