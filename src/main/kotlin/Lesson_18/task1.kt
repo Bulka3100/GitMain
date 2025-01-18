f648e2193ba71a119e7c660b196a1e4a74d2c882
Breaker <kirillvip3100@yandex.ru>
2025-01-18 16:20:51 +0300

    KS-18-1-ad-hoc

--- src/main/kotlin/Lesson_18/task1.kt
+++ src/main/kotlin/Lesson_18/task1.kt
@@ -1,2 +1,24 @@
 package org.example.Lesson_18
 
+
+fun main() {
+    val order = Order(1)
+    val order1 = Order(2)
+    order.printOrderInfo("ball")
+    order1.printOrderInfo(listOf<String>("eggs", "sugar"))
+}
+
+class Order(
+    val id: Int,
+
+    ) {
+    fun printOrderInfo(item: String) {
+        println("Заказан товар: $item")
+    }
+
+    fun printOrderInfo(items: List<String>) {
+        println("Заказаны товары: ${items.joinToString(", ")}")
+    }
+}
+// не нравится что не получилось создать свойства для типов товаров, чтобы изначально в конструктор писать товар, а потом уже выводить его содержимое по случаю
+
