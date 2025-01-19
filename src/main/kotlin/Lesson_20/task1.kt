b39d517b14bf93bbebf7133de567f4a15504e8a3
Breaker <kirillvip3100@yandex.ru>
2025-01-19 14:03:31 +0300

    KS-20-1

--- src/main/kotlin/Lesson_20/task1.kt
+++ src/main/kotlin/Lesson_20/task1.kt
@@ -1,2 +1,10 @@
 package org.example.Lesson_20
 
+fun main() {
+    println("Введите имя пользователя")
+    val userName = readln()
+    val splashGreetings: (String)-> Unit ={name:String-> println("С наступающим Новым Годом, $name!")}
+    splashGreetings(userName);
+    {it:String-> println("с новой годой $it") }("пользователь")
+
+}
