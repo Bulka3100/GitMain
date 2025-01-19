e2da68cc0ca24a43cbe443b44b88cd9dad574837
Breaker <kirillvip3100@yandex.ru>
2025-01-19 14:15:05 +0300

    KS-20-2-Player

--- src/main/kotlin/Lesson_20/task2.kt
+++ src/main/kotlin/Lesson_20/task2.kt
@@ -1,2 +1,16 @@
 package org.example.Lesson_20
 
+fun main() {
+    val player1= Player("Ivan",10,7);
+    println(player1.currentHp)
+    val healing = {player: Player-> player.currentHp+=3 }
+    healing(player1)
+    println(player1.currentHp)
+}
+class Player(
+    val name: String,
+    private val maxHP: Int,
+    var currentHp: Int,
+)
+//возникли вопросы, не совсем понимаю что по факту выводит лямбда. Ничего,просто производит действие, значит по сути Unit?
+//Также теперь интересно можно ли использовать лямбды в классах и нужно ли это вообще когда-то на практике. Спасибо)
