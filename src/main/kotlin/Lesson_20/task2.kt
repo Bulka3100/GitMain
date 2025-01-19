2ccff94e5c0a5716ccae6d4311a1e85f6914c672
Breaker <kirillvip3100@yandex.ru>
2025-01-19 14:15:22 +0300

    KS-20-2-Player

--- src/main/kotlin/Lesson_20/task2.kt
+++ src/main/kotlin/Lesson_20/task2.kt
@@ -1,12 +1,13 @@
 package org.example.Lesson_20
 
 fun main() {
-    val player1= Player("Ivan",10,7);
+    val player1 = Player("Ivan", 10, 7);
     println(player1.currentHp)
-    val healing = {player: Player-> player.currentHp+=3 }
+    val healing = { player: Player -> player.currentHp += 3 }
     healing(player1)
     println(player1.currentHp)
 }
+
 class Player(
     val name: String,
     private val maxHP: Int,
