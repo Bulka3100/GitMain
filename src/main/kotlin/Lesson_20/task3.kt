04fe497cb175e9feeacc09b5d9c809b1b96cef4a
Breaker <kirillvip3100@yandex.ru>
2025-01-19 16:21:20 +0300

    KS-20-3-Find-key

--- src/main/kotlin/Lesson_20/task3.kt
+++ src/main/kotlin/Lesson_20/task3.kt
@@ -1,2 +1,24 @@
 package org.example.Lesson_20
 
+fun main() {
+    val openDoor: (Player) -> Unit = { player: Player ->
+        if (player.hasKey == false)
+            println("дверь не поддаётся")
+        else println("дверь открыта")
+    }
+    val player1 = Player("Denis")
+    openDoor(player1)
+    player1.findKey()
+    openDoor(player1)
+
+}
+
+class Player(
+    val name: String,
+    var hasKey: Boolean = false
+) {
+    fun findKey() {
+        println("Игрок $name нашел ключ")
+        hasKey = true
+    }
+}
