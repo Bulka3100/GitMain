800d4843c5e41c78d1283c54ef001f339af487ed
Breaker <kirillvip3100@yandex.ru>
2025-01-19 18:23:21 +0300

    KS-20-4-questions

--- src/main/kotlin/Lesson_20/task4.kt
+++ src/main/kotlin/Lesson_20/task4.kt
@@ -1,2 +1,12 @@
 package org.example.Lesson_20
 
+fun main() {
+    val stringList: List<String> = listOf("кнопка1", "кнопка2", "кнопка3", "кнопка4")
+    val lambdaEl = stringList.mapIndexed { index, view -> { "нажат элемент $view" } }
+    println(lambdaEl) // плохо понимаю в чем ошибка GPT говорит что тут я создал не лямбды, но я не понимаю разницы
+    for ((index, lambda) in lambdaEl.withIndex()) {
+        if (index % 2 == 0)
+            println(lambda())
+    }
+
+}
