bc91e8960a8b3eae849307abff71577e65988365
Breaker <kirillvip3100@yandex.ru>
2025-01-20 20:20:09 +0300

    KS-21-2-sum

--- src/main/kotlin/Lesson_21/task2.kt
+++ src/main/kotlin/Lesson_21/task2.kt
@@ -1,2 +1,10 @@
 package org.example.Lesson_21
 
+fun main() {
+    val numbers = listOf<Int>(4, 5, 6, 7, 10)
+    println(numbers.evenNumbersSum())
+}
+
+fun List<Int>.evenNumbersSum(): Int {
+    return filter { it % 2 == 0 }.sum() //this.filter { it % 2 == 0 } эквивалентно
+}
