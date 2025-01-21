ea993705a0b6b20279c6fb42c5583c0cbd2d2bd9
Breaker <kirillvip3100@yandex.ru>
2025-01-21 09:39:17 +0300

    KS-21-3-Player-extension

--- src/main/kotlin/Lesson_21/task3.kt
+++ src/main/kotlin/Lesson_21/task3.kt
@@ -1,2 +1,20 @@
 package org.example.Lesson_21
 
+fun main() {
+    val player1 = Player("Adam", 10, 5)
+    val player2 = Player("Jenis", 10, 10)
+    println(player1.isHealthy())
+    println(player2.isHealthy())
+
+}
+
+class Player(
+    val name: String,
+    val maxHP: Int,
+    var currentHp: Int,
+)
+
+fun Player.isHealthy(): Boolean {
+    if (this.currentHp == this.maxHP) return true else return false
+
+}
