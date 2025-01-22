725c5646292f91e74188f585e6b850f665d21ed0
Breaker <kirillvip3100@yandex.ru>
2025-01-23 02:28:03 +0300

    KS-22-1-equals

--- src/main/kotlin/Lesson_22/task1.kt
+++ src/main/kotlin/Lesson_22/task1.kt
@@ -1,2 +1,22 @@
 package org.example.Lesson_22
 
+fun main() {
+    val regularBook1 = RegularBook("Автостопом по галактике", "Роман Адамс")
+    val regularBook2 = RegularBook("Автостопом по галактике", "Роман Адамс")
+    val dataBook1 = DataBook("Заводной апельсин", "Энтони Берджесс")
+    val dataBook2 = DataBook("Заводной апельсин", "Энтони Берджесс")
+    println(regularBook2 == regularBook1) //тут в сравнение идут не свойства объекта, их хэшкоды, которые различаются при создании (ссылки на объекты)
+    println(dataBook2 == dataBook1) // в датакллассе сравниваются именно данные объектов
+}
+
+class RegularBook(
+    val name: String,
+    val author: String,
+) {
+
+}
+
+data class DataBook(
+    val name: String,
+    val author: String,
+)
