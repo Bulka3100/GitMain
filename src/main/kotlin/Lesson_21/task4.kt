b6f441fc0c643c7fdf116aebec7ee01e7d83bbf8
Breaker <kirillvip3100@yandex.ru>
2025-01-22 20:40:27 +0300

    KS-21-4-need rework

--- src/main/kotlin/Lesson_21/task4.kt
+++ src/main/kotlin/Lesson_21/task4.kt
@@ -1,2 +1,14 @@
 package org.example.Lesson_21
 
+import java.io.File
+
+fun main() {
+
+}
+fun File.addWord() {
+    println("введите слово для добавления")
+    val word = readln()
+    val lowerCaseWord = word.lowercase() //среда зачеркивает .toLowerCase, но боты пишут что верный именно он. Так какой более актуальный?
+    writeText(lowerCaseWord)
+
+} // как будто мало и ещё не понимаю работы. Доработать
