55ce410d9a8c58b8f24cd44d518d395cd6bc9051
Breaker <kirillvip3100@yandex.ru>
2025-01-23 02:57:17 +0300

    KS-22-3-destructurization

--- src/main/kotlin/Lesson_22/task3.kt
+++ src/main/kotlin/Lesson_22/task3.kt
@@ -1,2 +1,17 @@
 package org.example.Lesson_22
 
+fun main() {
+    val player = Player(100, 50, 75)
+    val (damage1, hp1, mana1) = player
+    println(damage1)
+    println(hp1)
+    println(mana1)
+
+}
+
+data class Player(
+    val damage: Int,
+    val hp: Int,
+    val mana: Int,
+)
+//для пропуска символ _
