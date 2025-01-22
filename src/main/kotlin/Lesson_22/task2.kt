f1aac1fe6fa8e866c6a6eba803c86a02c8225c5a
Breaker <kirillvip3100@yandex.ru>
2025-01-23 02:38:12 +0300

    KS-22-2-toString

--- src/main/kotlin/Lesson_22/task2.kt
+++ src/main/kotlin/Lesson_22/task2.kt
@@ -1,8 +1,8 @@
 package org.example.Lesson_22
 
 fun main() {
-val regularBook= RegularBook("1984","Джордж Оруэл")
-    val  dataBook= DataBook("Фауст","Гётте")
+    val regularBook = RegularBook("1984", "Джордж Оруэл")
+    val dataBook = DataBook("Фауст", "Гётте")
     println(regularBook) //выдаёт строковое представление объекта по умолчанию
     println(dataBook) // при вызове println под копотом вызывается метод toString, в Data class он переопределён
 }
