d3cea877f3e9f6cf3edd69dbfc7cb3fbaf53af4b
Breaker <kirillvip3100@yandex.ru>
2025-01-23 05:01:42 +0300

    KS-22-4-logic-question

--- src/main/kotlin/Lesson_22/task4.kt
+++ src/main/kotlin/Lesson_22/task4.kt
@@ -1,2 +1,25 @@
 package org.example.Lesson_22
 
+fun main() {
+
+}
+class MainScreenViewModel(
+    val mainScreenState: MainScreenState
+) {
+    init {
+        if(mainScreenState.isLoading==true)
+    }
+    data class MainScreenState(
+        var data: String,
+        var isLoading: Boolean = false
+    )
+    fun loadData(){
+mainScreenState.data =noneData
+    }
+    companion object{
+        val noneData="- отсутствие данных;"
+        val loadData="- загрузка данных;"
+        val isData= "- наличие загруженных данных."
+    }
+}
+//TODO понять логику
