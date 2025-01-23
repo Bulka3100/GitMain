aec2320ac609797042044c2e775b7259f36633f1
Breaker <kirillvip3100@yandex.ru>
2025-01-23 06:33:15 +0300

    KS-6-1-refactor

--- src/main/kotlin/Lesson_6/task1.kt
+++ src/main/kotlin/Lesson_6/task1.kt
@@ -2,15 +2,15 @@ package org.example.Lesson_6
 
 fun main() {
     println("создайте пароль и логин")
-    var iaAutorizred = false
+    var isAutorizred = false
     val pasword = readLine()
     println("введи пароль")
     do {
         val passwordEnter = readLine()
-        if (passwordEnter != pasword) println("неверная авторизация") else continue
+        if (passwordEnter != pasword) println("неверная авторизация")
+        continue
     } while (passwordEnter != pasword)
     println("авторизация успешна")
-
 
 }
 
