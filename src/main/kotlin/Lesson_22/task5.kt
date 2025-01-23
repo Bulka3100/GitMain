14e519064695238c221c224edd4596872e0a8ccf
Breaker <kirillvip3100@yandex.ru>
2025-01-23 05:39:58 +0300

    KS-22-5-last-task

--- src/main/kotlin/Lesson_22/task5.kt
+++ src/main/kotlin/Lesson_22/task5.kt
@@ -1,2 +1,21 @@
 package org.example.Lesson_22
 
+import java.sql.Date
+
+fun main() {
+    val data: Date = Date(1)
+    val alpha_Centauri = GalacticGuide("Alpha Centauri", "Planet for living", data, 2000)
+    val (name, description, time, long) = alpha_Centauri
+    println(name)
+    println(description)
+    println(time)
+    println(long)
+}
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val time: Date,
+    val longInLightYears: Int
+)
+
