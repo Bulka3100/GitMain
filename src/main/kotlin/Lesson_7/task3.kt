7c75187c6e438ffce073515a474e97699c3e0618
Breaker <kirillvip3100@yandex.ru>
2025-01-23 07:56:31 +0300

    KS-7-3-refactor

--- src/main/kotlin/Lesson_7/task3.kt
+++ src/main/kotlin/Lesson_7/task3.kt
@@ -1,9 +1,7 @@
 package org.example.Lesson_7
 
 fun main() {
-    val x = readlnOrNull()!!.toInt()
-    val range = 0..x
-    for (i in range) {
-        if (i % 2 == 0) println(i) else continue
-    }
+    val number = readln().toInt()
+    val range = 0..number step 2
+    for (i in range) println(i)
 }
