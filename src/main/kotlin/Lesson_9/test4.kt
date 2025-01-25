bf1750fa33ecc5aae74c551ea7c487b55b69a6b3
Breaker <kirillvip3100@yandex.ru>
2025-01-25 22:38:29 +0300

    KS-9-4-refactor

--- src/main/kotlin/Lesson_9/test4.kt
+++ src/main/kotlin/Lesson_9/test4.kt
@@ -1,8 +1,9 @@
 package org.example.Lesson_9
 
 fun main() {
-  val ingredients=readln()
-   val input = ingredients.split(",")
+    println("введите 5 ингридиентов")
+    val ingredients = readln()
+    val input = ingredients.split(", ")
     val sorted = input.sorted()
     println("отсортированный список: $sorted")
 }
