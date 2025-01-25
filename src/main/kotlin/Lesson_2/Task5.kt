b02a1f4b6d52cd03367978747de0e53e953220ca
Breaker <kirillvip3100@yandex.ru>
2025-01-25 21:22:41 +0300

    KS-2-5-refactor

--- src/main/kotlin/Lesson_2/Task5.kt
+++ src/main/kotlin/Lesson_2/Task5.kt
@@ -5,9 +5,9 @@ import kotlin.math.pow
 
 fun main() {
     val summ = 70_000
-    val procent = 16.7
+    val percent = 16.7
     val time = 20
-    val result = (summ * ((1 + procent / 100).pow(time))).toDouble()
+    val result = (summ * ((1 + percent / 100).pow(time))).toDouble()
     val formated = ("%.3f".format(result))
     println(formated)
 
