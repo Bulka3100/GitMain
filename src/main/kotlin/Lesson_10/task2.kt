3ef094d14b7f08aa2fab7c82db0e0a9fe58e712a
Breaker <kirillvip3100@yandex.ru>
2025-01-26 00:18:25 +0300

    KS-10-2-refactor

--- src/main/kotlin/Lesson_10/task2.kt
+++ src/main/kotlin/Lesson_10/task2.kt
@@ -2,32 +2,18 @@ package org.example.Lesson_10
 
 fun main() {
     println("введите логин")
-    val login = gettt()
+    val login = get()
 
     println("введите пароль")
-    val password = gettt()
+    val password = get()
+    println("добрый вечер!")
 }
 
-/*fun valid(inputLength: String): Boolean {
-    return inputLength.length > 4
-}
-*/
-/*fun getValidInput(): String {
-    var input: String
+fun get(): String {
+    var loginOr: String
     do {
-        input = readln()
-        if (!valid(input)) {
-            println("Логин или пароль недостаточно длинные. Повторите попытку.")
-        }
-    } while (!valid(input))
-    return input
-}*/
-
-fun gettt(): String {
-    var login: String
-    do {
-        login = readln()
-        if(login.length<4) println("меньше 4х символов")
-    } while (login.length < 4)
-    return login
+        loginOr = readln()
+        if (loginOr.length < 4) println("меньше 4х символов")
+    } while (loginOr.length < 4)
+    return loginOr
 }
