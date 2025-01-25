9b6f846f8b52c137a5f678d8c3259b6cfd23dc1f
Breaker <kirillvip3100@yandex.ru>
2025-01-25 20:33:48 +0300

    KS-8-2-refactor

--- src/main/kotlin/Lesson_8/task2.kt
+++ src/main/kotlin/Lesson_8/task2.kt
@@ -2,9 +2,15 @@ package org.example.Lesson_8
 
 fun main() {
     val listOfIngredients = arrayOf("egg", "rice", "milk")
-    val ingredient = readlnOrNull()
+    val ingredient = readLine()
+    var counter = 0
     for (i in listOfIngredients) {
-        if (i == ingredient) println("есть в списке") else println("нет в списке")
-        break
+        if (i == ingredient) {
+            println("есть в списке")
+            counter += 1
+        } else counter += 0
+    }
+    if (counter==0){
+        println("нет в списке")
     }
 }
