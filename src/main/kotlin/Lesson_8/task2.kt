4444fad0c0df05e2eb579157435b225cd6745674
Breaker <kirillvip3100@yandex.ru>
2025-01-28 09:33:35 +0300

    KS-8-2-refactor

--- src/main/kotlin/Lesson_8/task2.kt
+++ src/main/kotlin/Lesson_8/task2.kt
@@ -3,14 +3,12 @@ package org.example.Lesson_8
 fun main() {
     val listOfIngredients = arrayOf("egg", "rice", "milk")
     val ingredient = readLine()
-    var counter = 0
+    var flag: Boolean = false
     for (i in listOfIngredients) {
         if (i == ingredient) {
             println("есть в списке")
-            counter += 1
-        } else counter += 0
+            flag = true
+        }
     }
-    if (counter==0){
-        println("нет в списке")
-    }
+    if (flag == false) println("нет в списке")
 }
