fb6e09a0b8c4bca57f7a665faff06b334982e6b3
Breaker <kirillvip3100@yandex.ru>
2025-01-28 09:44:00 +0300

    KS-8-4-refactor

--- src/main/kotlin/Lesson_8/task4.kt
+++ src/main/kotlin/Lesson_8/task4.kt
@@ -1,16 +1,17 @@
 package org.example.Lesson_8
 
-fun main() {
-    var arrayOfIngredients = arrayOf("лук", "чеснок", "чипсы", "паприка", "вермишель")
-    println("что заменить?")
-    val change = readlnOrNull()
-    if (change in arrayOfIngredients) {
-        println("что хотели бы добавить?")
-        val changeOn = readln()
-        arrayOfIngredients = arrayOfIngredients.filterNot { it == change }.toTypedArray()
-        arrayOfIngredients += changeOn
-        println(arrayOfIngredients.joinToString())
-    } else if (change !in arrayOfIngredients) {
-        println("нет такого в списке")
-    } else println("ошибка")
-}
+    fun main() {
+        var arrayOfIngredients = arrayOf("лук", "чеснок", "чипсы", "паприка", "вермишель")
+        println("Что заменить?")
+        val change = readlnOrNull()
+            val index = arrayOfIngredients.indexOf(change)
+
+            if (index >= 0) {
+                println("Что хотели бы добавить?")
+                val changeOn = readln()
+                arrayOfIngredients[index] = changeOn
+                println(arrayOfIngredients.joinToString())
+            } else {
+                println("Нет такого в списке")
+            }
+    }
