ffc4b34f00ffcee561dfc4938e8cfb8524177728
Breaker <kirillvip3100@yandex.ru>
2025-01-29 08:44:08 +0300

    KS-9-1-new

--- src/main/kotlin/Lesson_10/task1.kt
+++ src/main/kotlin/Lesson_10/task1.kt
@@ -1,3 +1,7 @@
 package org.example.Lesson_10
 
-fun main() {}
+fun main() {
+    val ingredients = listOf<String>("лук","картошка","молоко")
+    println("в рецепте есть следующие ингредиенты: $ingredients")
+    ingredients.forEach{println(it)}
+}
