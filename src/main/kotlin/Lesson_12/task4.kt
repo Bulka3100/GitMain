a2af2d24c851181c166aedf06f2b9cc98381f356
Breaker <kirillvip3100@yandex.ru>
2025-01-29 14:34:39 +0300

    KS-12-4

--- src/main/kotlin/Lesson_12/task4.kt
+++ src/main/kotlin/Lesson_12/task4.kt
@@ -1,3 +1,24 @@
 package org.example.Lesson_12
 
-fun main() {}
+fun main() {
+
+    val November15 = Weather(
+        _dayTemp = 13,
+        _nightTemp = 11,
+    )
+}
+
+class Weather(
+    _dayTemp: Int,
+    _nightTemp: Int,
+) {
+    var dayTemp=_dayTemp
+    var nightTemp=_nightTemp
+    init {
+        println("днём $dayTemp вечером $nightTemp")
+    }
+    fun showAll() {
+        println("днём $dayTemp вечером $nightTemp")
+
+    }
+}
