63cd2144986e74eee0064a8b3ba3241ce05ece13
Breaker <kirillvip3100@yandex.ru>
2025-01-29 14:04:40 +0300

    KS-9-5-good

--- src/main/kotlin/Lesson_9/test5.kt
+++ src/main/kotlin/Lesson_9/test5.kt
@@ -1,17 +1,8 @@
 package org.example.Lesson_9
 
 fun main() {
-    val ingredients = mutableSetOf<String>()
-    val first = readln().uppercase()
-    val second = readln()
-    val third = readln()
-    val fourth = readln()
-    val fifth = readln()
-    ingredients.add(first)
-    ingredients.add(second)
-    ingredients.add(third)
-    ingredients.add(fourth)
-    ingredients.add(fifth)
-    val sorted = ingredients.toSortedSet()
-    println(sorted.joinToString())
+    val ingredients = List(5) { readln() }
+    val withoutRepeat = ingredients.toSet()
+    val result = withoutRepeat.mapIndexed { index, element -> if (index == 0) element.capitalize() else element }
+    println(result.joinToString())
 }
