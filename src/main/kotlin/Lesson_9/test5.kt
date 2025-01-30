b7105b46e2d3ec758430307bd6aa9c21de352fbc
Breaker <kirillvip3100@yandex.ru>
2025-01-30 19:59:12 +0300

    KS-9-5

--- src/main/kotlin/Lesson_9/test5.kt
+++ src/main/kotlin/Lesson_9/test5.kt
@@ -5,6 +5,7 @@ package org.example.Lesson_9
 fun main() {
     val ingredients = List(5) { readln() }
     val withoutRepeat = ingredients.toSet()
-    val result = withoutRepeat.mapIndexed { index, element -> if (index == 0) element.replaceFirstChar { it.uppercase() } else element }
+    val result =
+        withoutRepeat.mapIndexed { index, element -> if (index == 0) element.replaceFirstChar { it.uppercase() } else element }
     println(result.joinToString())
 }
