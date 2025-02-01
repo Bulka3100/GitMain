028b3aa8146e46abf60b2c9707611ea9dab90a93
Breaker <kirillvip3100@yandex.ru>
2025-02-01 15:11:26 +0300

    KS-10-4-refactor

--- src/main/kotlin/Lesson_10/task4.kt
+++ src/main/kotlin/Lesson_10/task4.kt
@@ -5,9 +5,11 @@ fun main() {
     do {
         println("хотите бросить кости еще раз?")
         val question = readln()
-        if (question.equals("да", ignoreCase = true)){ println(goRound())}
-        else if (question.equals("нет", ignoreCase = true)){ println("количество побед:winCounter как сюда вернуть")}
-        else println("нет такого варианта")
+        if (question.equals("да", ignoreCase = true)) {
+            println(goRound())
+        } else if (question.equals("нет", ignoreCase = true)) {
+            println("количество побед:winCounter как сюда вернуть")
+        } else println("нет такого варианта")
     } while (question.equals("да", ignoreCase = true))
 }
 
@@ -26,7 +28,10 @@ fun goRound(): String {
     else if (Robot < Human) {
         return ("Human")
         winCounter += 1
-    } else return ("ничья")
+    } else {
+        return ("ничья")
+    }
 }
+
 const val USUAL_DUST_FIRST = 1
 const val USUAL_DUST_LAST = 6
