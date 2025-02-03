ccfe1fb7fd54f3354f567abf5667727cb8eaa801
Breaker <kirillvip3100@yandex.ru>
2025-02-03 10:52:26 +0300

    KS-10-5-corrected

--- src/main/kotlin/Lesson_10/task5.kt
+++ src/main/kotlin/Lesson_10/task5.kt
@@ -2,17 +2,18 @@ package org.example.Lesson_10
 
 
 fun main() {
-val myToken=autorization("Bulka31","QWERTY")
+    val myToken = autorization("Bulka31", "QWERTY")
     println(getBasket(myToken))
 }
-fun generateToken(): String{
+
+fun generateToken(): String {
     val chars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     return (1..32).map { chars.random() }.joinToString("")
 }
 
 fun autorization(login: String, password: String): String? {
     if (login == LOGIN && password == PASSWORD) {
-            val token=generateToken()
+        val token = generateToken()
         return token
     } else {
         return null
@@ -21,10 +22,9 @@ fun autorization(login: String, password: String): String? {
 
 private val buy: List<String> = listOf<String>("хлопья", "молоко", "мармелад")
 fun getBasket(token: String?): List<String>? {
-if (token==autorization(LOGIN,PASSWORD)){
-    return buy
-}
-    else {
+    if (token == autorization(LOGIN, PASSWORD)) {
+        return buy
+    } else {
         return null
     }
 
