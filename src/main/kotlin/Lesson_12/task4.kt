4b0f9201f12ea34222765cdb0854293259ab2725
Breaker <kirillvip3100@yandex.ru>
2025-02-04 02:20:54 +0300

    KS-11-4-corrected

--- src/main/kotlin/Lesson_12/task4.kt
+++ src/main/kotlin/Lesson_12/task4.kt
@@ -3,8 +3,8 @@ package org.example.Lesson_12
 fun main() {
 
     val November15 = Weather(
-        _dayTemp = 13,
-        _nightTemp = 11,
+        _dayTemp = 278,
+        _nightTemp = 274,
     )
 }
 
@@ -12,11 +12,13 @@ class Weather(
     _dayTemp: Int,
     _nightTemp: Int,
 ) {
-    var dayTemp=_dayTemp
-    var nightTemp=_nightTemp
+    var dayTemp = _dayTemp - 273
+    var nightTemp = _nightTemp - 273
+
     init {
-        println("днём $dayTemp вечером $nightTemp")
+        showAll()
     }
+
     fun showAll() {
         println("днём $dayTemp вечером $nightTemp")
 
