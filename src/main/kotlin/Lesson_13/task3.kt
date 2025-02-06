20d79dc1b85610acd521d0f354f5e65a752ae478
Breaker <kirillvip3100@yandex.ru>
2025-02-06 23:05:50 +0300

    KS-13-3-refactor

--- src/main/kotlin/Lesson_13/task3.kt
+++ src/main/kotlin/Lesson_13/task3.kt
@@ -2,32 +2,32 @@ package org.example.Lesson_13
 
 fun main() {
     val client1 = Client(
-        name="abob",
-        number =1349494949,
+        name = "abob",
+        number = 1349494949,
         company = null
     )
     val client2 = Client(
-        name="john",
-        number =9909494949,
+        name = "john",
+        number = 9909494949,
         company = "null"
     )
     val client3 = Client(
-        name="alex",
-        number =5549494949,
+        name = "alex",
+        number = 5549494949,
         company = "sky"
     )
     val client4 = Client(
-        name="arut",
-        number =559494949,
+        name = "arut",
+        number = 559494949,
         company = "sorlex"
     )
     val client5 = Client(
-        name="slonnik",
-        number =9509494949,
+        name = "slonnik",
+        number = 9509494949,
         company = null
     )
     val companiesList = listOf<Client>(client1, client2, client3, client4, client5)
-    val companies = companiesList.mapNotNull{ it.company }
+    val companies = companiesList.mapNotNull { it.company }
     println(companies)
 }
 
