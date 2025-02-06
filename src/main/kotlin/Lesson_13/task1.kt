5cce8555bd98fa64342bf1ae058896befb642484
Breaker <kirillvip3100@yandex.ru>
2025-02-06 22:54:55 +0300

    KS-13-1-refactor

--- src/main/kotlin/Lesson_13/task1.kt
+++ src/main/kotlin/Lesson_13/task1.kt
@@ -2,8 +2,9 @@ package org.example.Lesson_13
 
 fun main() {
 }
+
 class ClientData(
     val name: String,
     val number: Long,
-    val company: String?=null ,//но ведь это инициализация по умолчанию?
+    val company: String?,
 )
