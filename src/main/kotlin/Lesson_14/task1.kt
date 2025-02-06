1b09227a2341257d5956bb4302b1420c4639821e
Breaker <kirillvip3100@yandex.ru>
2025-02-06 11:27:32 +0300

    KS-14-1-corrected

--- src/main/kotlin/Lesson_14/task1.kt
+++ src/main/kotlin/Lesson_14/task1.kt
@@ -3,15 +3,17 @@ package org.example.Lesson_14
 fun main() {
 
     val liner = Liner()
-    val load = Load() //я хочтел изменять только некоторые свойства класса чтобы остальные передовались по дефолту из родителя
+    val load = Load()
     val iceBreaker = IceBreaker()
     println(load.name)
+    println(load.personCapacity)
+    println(liner.name)
     println(load.speed)
     println(load.loadCapacity)
     println(iceBreaker.iceBreak())
     println(iceBreaker.runSystemDiagnostic())
     println(iceBreaker.speed)
-    println(load.runSystemDiagnostic()) //почему выдает kotlinUnit?
+    println(load.runSystemDiagnostic())
 
 }
 
@@ -21,8 +23,8 @@ open class Liner(
     val loadCapacity: Int = 120,
     val personCapacity: Int = 10,
 ) {
-    open fun runSystemDiagnostic() {
-        println("$name: проверены системы коробля ")
+    open fun runSystemDiagnostic(): String {
+        return "$name: проверены системы коробля"
     }
 }
 
@@ -30,15 +32,15 @@ class Load(
     name: String = "грузовой",
     speed: Int = 80,
     loadCapacity: Int = 120,
-) : Liner(name, speed = speed, loadCapacity = loadCapacity)
-// почему при добавлении логики в init все ломается?
+) : Liner(name, speed, loadCapacity)
+
 
 class IceBreaker(
     name: String = "ледокол",
     speed: Int = 70,
     loadCapacity: Int = 40,
-) : Liner(name, speed = speed, loadCapacity = loadCapacity) {
-    fun iceBreak() {
-        println("лед расколот")
+) : Liner(name, speed, loadCapacity) {
+    fun iceBreak(): String {
+        return "лед расколот"
     }
 }
