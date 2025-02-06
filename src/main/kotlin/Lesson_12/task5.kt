84cd5daecf56a2d4341310e61a2442cc015376fa
Breaker <kirillvip3100@yandex.ru>
2025-02-06 09:53:59 +0300

    KS-12-5-corrected

--- src/main/kotlin/Lesson_12/task5.kt
+++ src/main/kotlin/Lesson_12/task5.kt
@@ -5,23 +5,18 @@ import kotlin.random.Random
 
 fun main() {
     val weatherList = mutableListOf<Weather>()
-
     for (i in 1..30) {
-        val dayTemp: Int = Random.nextInt(10, 40)
-        val nightTemp: Int = Random.nextInt(8, 30)
+        val dayTemp: Int = Random.nextInt(270, 293)
+        val nightTemp: Int = Random.nextInt(269, 290)
         val isRain: Boolean = Random.nextBoolean()
         weatherList.add(Weather(dayTemp, nightTemp, isRain))
+
     }
     val night = weatherList.map { it.dayTemp }
     val day = weatherList.map { it.nightTemp }
-    val rain = weatherList.count{it.isRain}
-    println(night)
-    println(day)
-}
+    val rain = weatherList.count { it.isRain }
+    println(weatherList.map { (it.dayTemp + it.nightTemp) / 2 }.average())
 
-fun Iterable<Int>.calculateAverage(): Double {
-    if (isEmpty()) return 0.0
-    return sum().toDouble() / size
 }
 
 class Weather(
@@ -29,12 +24,12 @@ class Weather(
     _nightTemp: Int,
     _isRain: Boolean,
 ) {
-    var dayTemp = _dayTemp
-    var nightTemp = _nightTemp
+    var dayTemp = _dayTemp - 273
+    var nightTemp = _nightTemp - 273
     var isRain = _isRain
 
     fun showAll() {
         println("днём $dayTemp вечером $nightTemp")
 
     }
-}//в блоке инит у нас происходит любой код что мы напишем и он выполняется когда мы создаем объект?
+}
