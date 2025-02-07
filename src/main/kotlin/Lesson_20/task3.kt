592e01b291765e5cb0409c9e786ee9f48580f3cd
Breaker <kirillvip3100@yandex.ru>
2025-02-08 01:31:24 +0300

    KS-20-3

--- src/main/kotlin/Lesson_20/task3.kt
+++ src/main/kotlin/Lesson_20/task3.kt
@@ -1,16 +1,17 @@
 package org.example.Lesson_20
 
 fun main() {
-    val openDoor: (Player) -> Unit = { player: Player ->
-        if (player.hasKey == false)
-            println("дверь не поддаётся")
-        else println("дверь открыта")
-    }
     val player1 = Player("Denis")
     openDoor(player1)
     player1.findKey()
     openDoor(player1)
 
+}
+
+val openDoor: (Player) -> Unit = { player: Player ->
+    if (player.hasKey == false)
+        println("дверь не поддаётся")
+    else println("дверь открыта")
 }
 
 class Player(
