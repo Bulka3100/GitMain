5b6e8cebab309597bc74be884a79061ed68317e6
Breaker <kirillvip3100@yandex.ru>
2025-02-08 20:25:01 +0300

    KS-21-4--refactor

--- src/main/kotlin/Lesson_21/task4.kt
+++ src/main/kotlin/Lesson_21/task4.kt
@@ -3,9 +3,10 @@ package org.example.Lesson_21
 import java.io.File
 
 fun main() {
-val newFile= File("new.txt")
+    val newFile = File("new.txt")
     newFile.addWord()
 }
+
 fun File.addWord() {
     println("введите слово для добавления")
     val word = readln()
