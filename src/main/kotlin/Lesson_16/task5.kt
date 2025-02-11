ddb6957d5ea425fda00234cb232a5783e661cd25
Breaker <kirillvip3100@yandex.ru>
2025-02-11 23:52:52 +0300

    KS-16-5-refactor

--- src/main/kotlin/Lesson_16/task5.kt
+++ src/main/kotlin/Lesson_16/task5.kt
@@ -1,20 +1,25 @@
 package org.example.Lesson_16
 
 fun main() {
-    val player = Player("Hero", 10, 5)
+    val player = Player("Hero", 10, 5, false)
     player.getDamage()
     player.getDamage()
     player.heal()
     player.getDamage()
-
+    player.getDamage()
+    player.getDamage()
+    player.getDamage()
+    player.heal()
+    player.getDamage()
 }
 
 class Player(
     val name: String,
     private var hp: Int = 10,
     var punchPower: Int,
+    var isDead: Boolean
 
-    ) {
+) {
     init {
         require(hp > 0) { "хп больше 0" }
         println("удачного приключения!")
@@ -22,7 +27,7 @@ class Player(
 
     fun heal() {
         if (hp > 0 && hp < maxHeal) {
-            val ininialHp = hp
+            val initialHp = hp
             hp += healValue
             println("${name}: лечение hp=$hp")
         } else return
@@ -30,17 +35,19 @@ class Player(
     }
 
     fun getDamage() {
-        hp -= atackValue
-        println("${name}: получен урон hp=$hp")
-        if (hp <= 0) {
-            dead()
+        if (isDead == false) {
+            hp -= atackValue
+            println("${name}: получен урон hp=$hp")
+            if (hp <= 0) {
+                dead()
+            } else println("герой мертв!")
         }
     }
 
     private fun dead() {
-
         println("${name}: умер")
         punchPower = 0
+        isDead = true
 
     }
 
