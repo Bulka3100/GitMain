bf4555a97fed2e2bb31fe8d616854d440bb49fce
Breaker <kirillvip3100@yandex.ru>
2025-02-12 02:56:32 +0300

    KS-19-5-refactor

--- src/main/kotlin/Lesson_19/task.5.kt
+++ src/main/kotlin/Lesson_19/task.5.kt
@@ -1,22 +1,32 @@
 package org.example.Lesson_19
 
 fun main(){
-    println("Добро пожаловать пользователь. Вбей сначала имя, затем пол")
     val humanList= mutableListOf<Human>()
+    for (i in 1..2){
+    println("Добро пожаловать пользователь. Вбей сначала имя, затем пол")
     val name= readln()
-    val sex: Sex=readln()
-    val human1= Human(name,sex)
-    val human2= Human(name,sex)
-    val human3= Human(name,sex)
-    val human4=Human(name,sex)
-    val human5=Human(name,sex)
-    humanList.add(human5)
+
+        var sex: Sex? = null
+        while (sex == null) {
+            println("Введите пол (MALE, FEMALE, ANOTHER):")
+            val input = readln().uppercase()
+
+            sex = try {
+                Sex.valueOf(input)
+            } catch (e: IllegalArgumentException) {
+                println("Неверный ввод. Попробуйте еще раз.")
+                null
+            }
+        }
+    humanList+= Human(name,sex)
+}
+    println(humanList.joinToString(", "){"${it.name},${it.sex}"})
 
 }
 
-class Human(
-    name: String,
-    sex: Sex,
+data class Human(
+    val name: String,
+   val  sex: Sex,
 )
 enum class Sex{
     MALE,
