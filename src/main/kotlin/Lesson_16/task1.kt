c2b6d5e685f1e3fc7b2459858d211a2bd4f43073
Breaker <kirillvip3100@yandex.ru>
2025-02-11 08:06:42 +0300

    KS-16-1-refactor

--- src/main/kotlin/Lesson_16/task1.kt
+++ src/main/kotlin/Lesson_16/task1.kt
@@ -1,14 +1,11 @@
 package org.example.Lesson_16
 
-import kotlin.random.Random
-
 fun main() {
     val dust = Dust()
     dust.printInfo()
 }
-
 class Dust(
-    private val number: Int = Random.nextInt(1, 7)
+    private val number: Int = (1..6).random()
 ) {
     fun printInfo() {
         println(number)
