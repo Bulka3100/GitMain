ddafe0e80f5491b267d3c55a5e827f8f981760ce
Breaker <kirillvip3100@yandex.ru>
2025-02-12 21:41:50 +0300

    KS-22-5-refactor

--- src/main/kotlin/Lesson_22/task5.kt
+++ src/main/kotlin/Lesson_22/task5.kt
@@ -1,9 +1,10 @@
 package org.example.Lesson_22
 
 import java.sql.Date
+import java.time.LocalDateTime
 
 fun main() {
-    val data: Date = Date(1)
+    val data = LocalDateTime.of(2023, 10, 15, 12, 0)
     val alpha_Centauri = GalacticGuide("Alpha Centauri", "Planet for living", data, 2000)
     val (name, description, time, long) = alpha_Centauri
     println(name)
@@ -16,7 +17,7 @@ fun main() {
 data class GalacticGuide(
     val name: String,
     val description: String,
-    val time: Date,
+    val time: LocalDateTime,
     val longInLightYears: Int
 )
 
