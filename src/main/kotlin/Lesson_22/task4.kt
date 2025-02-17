86f0e21c25c0460b105e20c1b974c7bb09f45e57
Breaker <kirillvip3100@yandex.ru>
2025-02-17 07:23:31 +0300

    KS-22-4-refactor

--- src/main/kotlin/Lesson_22/task4.kt
+++ src/main/kotlin/Lesson_22/task4.kt
@@ -1,33 +1,33 @@
 package org.example.Lesson_22
 
 fun main() {
+    val viewModel = MainScreenViewModel()
+    viewModel.loadData()
+}
+
+class MainScreenViewModel {
+    data class MainScreenState(var data: String? = null, val isLoading: Boolean = false)
+
+    var mainScreenState: MainScreenState = MainScreenState()
+    fun loadData() {
+        mainScreenState = mainScreenState.copy(data = State.NONE.data, isLoading = false)
+        println("текущее состояние $mainScreenState")
+        Thread.sleep(3000)
+        mainScreenState = mainScreenState.copy(data = State.LOAD.data, isLoading = true)
+        println("текущее состояние $mainScreenState")
+        Thread.sleep(3000)
+        mainScreenState = mainScreenState.copy(data = State.ISDATA.data, isLoading = false)
+        println("текущее состояние $mainScreenState")
+        println("данные загружены")
+
+
+    }
 
 }
 
-class MainScreenViewModel(
-    val mainScreenState: MainScreenState
-) {
-     class MainScreenState(data: String,isLoading: Boolean){
-        val data: String = data
-            set(value){
-                field=value
-            }
-        var isLoading: Boolean = isLoading
-    }
-
-
-
-    fun loadData(state: State) {
-        when(state){
-
-    }}
-
-
-    data class State(
-        val noneData: String = "- отсутствие данных",
-        val loadData: String = "- загрузка данных;",
-        val isData: String = "- наличие загруженных данных.",
-    )
-
-    }
+enum class State(val data: String) {
+    NONE("- отсутствие данных."),
+    LOAD("- загрузка данных."),
+    ISDATA("- наличие загруженных данных.")
+}
 
